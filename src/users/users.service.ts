b52b3dc9f7923dfae6f2957e3d8a59bf0e409971
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-06 22:25:04 +0500

    Update users.service.ts "add async to all functions"

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,13 +4,13 @@ import { UsersRepository } from './users.repository';
 @Injectable()
 export class UsersService {
   constructor(protected usersRepository: UsersRepository) {}
-  getUsers(query: { term: string }) {
+  async getUsers(query: { term: string }) {
     return this.usersRepository.getUsers(query);
   }
-  getUser(userId: string) {
+  async getUser(userId: string) {
     return this.usersRepository.getUser(userId);
   }
-  updateUser(id: number, inputModel) {
+  async updateUser(id: number, inputModel) {
     return this.usersRepository.updateUser(id, inputModel);
   }
 }
