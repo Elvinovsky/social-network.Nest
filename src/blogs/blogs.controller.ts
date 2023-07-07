b32d2ba0cde84535f5525429a1bd6b3e0ded3cdc
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-07 22:48:06 +0500

    Create blogs.controller.ts "added getBlogs method"

---
+++ src/blogs/blogs.controller.ts
@@ -0,0 +1,52 @@
+import { Controller, Get, HttpCode, HttpStatus, Query } from '@nestjs/common';
+import {
+  QueryInputModel,
+  SearchNameTerm,
+} from '../pagination/pagination.models';
+import { BlogsQueryRepo } from './blogs.query.repo';
+
+class PostsService {}
+
+@Controller('blogs')
+export class BlogsController {
+  constructor(
+    private readonly blogsQueryRepo: BlogsQueryRepo,
+    // protected blogsService: BlogsService,
+    private readonly postsService: PostsService,
+  ) {}
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async getBlogs(@Query() query: QueryInputModel & SearchNameTerm) {
+    return await this.blogsQueryRepo.getAllBlogs(query);
+  }
+  // @Get(':id')
+  // @HttpCode(HttpStatus.OK)
+  // async getBlog(@Param('id') blogId: string) {
+  //   const blog = await this.blogsService.findBlogById(blogId);
+  //   if (!blog) {
+  //     res.sendStatus(404);
+  //     return;
+  //   }
+  //   res.send(getByIdBlog);
+  //   return;
+  // }
+  // async getPostsByBlog(
+  //   req: RequestParamsAndInputQuery<{ blogId: string }, QueryInputModel>,
+  //   res: ResponseViewBody<PaginatorType<PostView[]>>,
+  // ) {
+  //   const getByBlogIdPosts = await this.blogsQueryRepo.searchPostByBlogId(
+  //     req.params.blogId,
+  //     Number(req.query.pageNumber),
+  //     Number(req.query.pageSize),
+  //     req.query.sortBy,
+  //     req.query.sortDirection,
+  //     req.user?.id,
+  //   );
+  //
+  //   if (!getByBlogIdPosts) {
+  //     res.sendStatus(404);
+  //     return;
+  //   }
+  //   res.send(getByBlogIdPosts);
+  // }
+}
