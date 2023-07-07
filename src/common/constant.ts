59a9a7f2beb11a38384c86199988248d44c38691
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-07 23:07:47 +0500

    Create constant.ts `added enums and default values by pagination`

---
+++ src/common/constant.ts
@@ -0,0 +1,8 @@
+export enum SortDirection {
+  Asc = 1,
+  Desc = -1,
+}
+
+export const DEFAULT_PAGE_SortBy = 'createdAt';
+export const DEFAULT_PAGE_NUMBER = 1;
+export const DEFAULT_PAGE_SIZE = 10;
