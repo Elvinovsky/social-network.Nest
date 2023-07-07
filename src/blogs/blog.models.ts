4d81bcb51337328a59445c55723d9ebc92e7e6f4
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-07 18:53:20 +0500

    Create blog.models.ts "added class 'BlogInputModel', types 'BlogCreateDTO', 'BlogViewModel' " Update package.json "dependencies injected "class-transformer", "class-validator""

---
+++ src/blogs/blog.models.ts
@@ -0,0 +1,46 @@
+import { IsNotEmpty, IsUrl, Length } from 'class-validator';
+
+export class BlogInputModel {
+  /**
+   * name input Blog {maxLength: 15}
+   */
+  @IsNotEmpty()
+  @Length(3, 15)
+  name: string;
+  /**
+   * description input model {maxLength: 500}
+   */
+  @IsNotEmpty()
+  @Length(5, 500)
+  description: string;
+  /**
+   * websiteUrl input model {maxLength: 100}
+   */
+  @IsNotEmpty()
+  @IsUrl()
+  @Length(4, 100)
+  websiteUrl: string;
+}
+
+export type BlogCreateDTO = {
+  name: string;
+  description: string;
+  websiteUrl: string;
+  addedAt: string;
+  /**
+   * True if user has not expired membership subscription to blog
+   */
+  isMembership: boolean;
+};
+
+export type BlogViewModel = {
+  id: string;
+  name: string;
+  description: string;
+  websiteUrl: string;
+  createdAt: string;
+  /**
+   * True if user has not expired membership subscription to blog
+   */
+  isMembership: boolean;
+};
