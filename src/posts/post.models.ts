93f250257dd7f64c4298905ed5666e61d0ff2801
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-09 19:12:07 +0500

    Create post.models.ts 'added classes "PostInputModel", "BlogPostInputModel", "PostCreateDTO", "PostViewDTO", "ExtendedLikesInfoView", "LikeInfoView", "LikeDBInfo"'

---
+++ src/posts/post.models.ts
@@ -0,0 +1,90 @@
+import { IsNotEmpty, Length } from 'class-validator';
+
+export class PostInputModel {
+  /**
+   * title input  model {maxLength: 30 }
+   */
+  @IsNotEmpty()
+  @Length(3, 30)
+  title: string;
+  /**
+   * shortDescription input model {maxLength: 100}
+   */
+  @IsNotEmpty()
+  @Length(3, 100)
+  shortDescription: string;
+  /**
+   * content input model {maxLength: 1000}
+   */
+  @IsNotEmpty()
+  @Length(3, 1000)
+  content: string;
+  /**
+   * ID existing Blog {linked to a post}
+   */
+  @IsNotEmpty()
+  blogId: string;
+}
+
+export class BlogPostInputModel {
+  // todo не использовать
+  /**
+   * title input  model {maxLength: 30 }
+   */
+  @IsNotEmpty()
+  @Length(3, 30)
+  title: string;
+  /**
+   * shortDescription input model {maxLength: 100}
+   */
+  @IsNotEmpty()
+  @Length(3, 100)
+  shortDescription: string;
+  /**
+   * content input model {maxLength: 1000}
+   */
+  @IsNotEmpty()
+  @Length(3, 1000)
+  content: string;
+}
+
+export type PostCreateDTO = {
+  title: string;
+  shortDescription: string;
+  content: string;
+  blogId: string;
+  blogName: string;
+  addedAt: string;
+};
+
+export type PostViewDTO = {
+  id: string;
+  title: string;
+  shortDescription: string;
+  content: string;
+  blogId: string;
+  blogName: string;
+  createdAt: string;
+  extendedLikesInfo: ExtendedLikesInfoView;
+};
+
+export type ExtendedLikesInfoView = {
+  likesCount: number;
+  dislikesCount: number;
+  myStatus: string;
+  newestLikes: LikeInfoView[] | null;
+};
+
+export type LikeInfoView = {
+  addedAt: string;
+  userId: string;
+  login: string;
+};
+
+export type LikeDBInfo = {
+  status: string;
+  userId: string;
+  userLogin: string;
+  postOrCommentId: string;
+  createdAt: Date;
+};
