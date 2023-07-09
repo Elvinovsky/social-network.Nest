f726a3617d424c7b9856719379553ee93e83c488
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-09 23:20:37 +0500

    Update blogs.service.ts "not realized"

--- src/blogs/blogs.service.ts
+++ src/blogs/blogs.service.ts
@@ -1,34 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { BlogViewDTO } from './blog.models';
+import { InjectModel } from '@nestjs/mongoose';
+import { Blog, BlogModel } from './blog.schemas';
+
 //output  view model
-@Injectable()
-export class BlogsService {
-  async createBlog(
-    name: string,
-    description: string,
-    websiteUrl: string,
-  ): Promise<BlogView> {
-    const createdBlog: BlogDBModel = {
-      name: name,
-      description: description,
-      websiteUrl: websiteUrl,
-      createdAt: new Date().toString(),
-      isMembership: false,
-    };
-
-    return await blogsRepository.addNewBlog(createdBlog);
-  }
-
-  async updateBlogById(
-    id: string,
-    name: string,
-    description: string,
-    websiteUrl: string,
-  ): Promise<boolean> {
-    return blogsRepository.updateBlogById(id, name, description, websiteUrl);
-  }
-
-  async BlogByIdDelete(id: string): Promise<boolean> {
-    return blogsRepository.searchBlogByIdDelete(id);
-  }
-}
+// @Injectable()
+// export class BlogsService {
+//   constructor(@InjectModel(Blog.name) private blogModel: BlogModel) {}
+//   async createBlog(
+//     name: string,
+//     description: string,
+//     websiteUrl: string,
+//   ): Promise<BlogViewDTO> {
+//     const createdBlog: Blog = {
+//       name: name,
+//       description: description,
+//       websiteUrl: websiteUrl,
+//       addedAt: new Date().toString(),
+//       isMembership: false,
+//     };
+//
+//     return await blogsRepository.addNewBlog(createdBlog);
+//   }
+//
+//   async updateBlogById(
+//     id: string,
+//     name: string,
+//     description: string,
+//     websiteUrl: string,
+//   ): Promise<boolean> {
+//     return blogsRepository.updateBlogById(id, name, description, websiteUrl);
+//   }
+//
+//   async BlogByIdDelete(id: string): Promise<boolean> {
+//     return blogsRepository.searchBlogByIdDelete(id);
+//   }
+// }
