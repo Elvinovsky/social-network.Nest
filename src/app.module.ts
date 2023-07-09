9d9ea980f0158110d5dcd0fad077ce01beb2f646
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-09 23:17:38 +0500

    Update app.module.ts "fix"

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,15 +11,34 @@ import { User, UserSchema } from './users/users.schema';
 import { BlogsController } from './blogs/blogs.controller';
 import { BlogsQueryRepo } from './blogs/blogs.query.repo';
 import { Blog, BlogSchema } from './blogs/blog.schemas';
-
+import { LikesQueryRepo } from './likes/likes.query.repo';
+import { LikesInfoRepository } from './likes/likes.repository';
+import { PostsMapping } from './posts/post.helpers';
+import { LikeAndDisQuantity } from './likes/like.helpers';
+import { Like, LikeSchema, Post, PostSchema } from './posts/post.schemas';
+const Mongo_Uri = process.env.MONGO_URL;
+if (!Mongo_Uri) {
+  throw new Error('not db connect');
+}
 @Module({
   imports: [
     configModule,
-    MongooseModule.forRoot(process.env.MONGO_URL),
+    MongooseModule.forRoot(Mongo_Uri),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     MongooseModule.forFeature([{ name: Blog.name, schema: BlogSchema }]),
+    MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
+    MongooseModule.forFeature([{ name: Like.name, schema: LikeSchema }]),
   ],
   controllers: [AppController, UsersController, BlogsController],
-  providers: [AppService, UsersService, UsersRepository, BlogsQueryRepo],
+  providers: [
+    AppService,
+    UsersService,
+    UsersRepository,
+    BlogsQueryRepo,
+    LikesQueryRepo,
+    LikesInfoRepository,
+    PostsMapping,
+    LikeAndDisQuantity,
+  ],
 })
 export class AppModule {}
