456e046e4d08d54bfaa54b80c9d663fe677597bb
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-09 19:13:34 +0500

    Create post.schemas.ts 'added classes "Post", "LikeDBInfo", types "LikeDBInfoDocument", "PostSchema"'

---
+++ src/posts/post.schemas.ts
@@ -0,0 +1,53 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, Model } from 'mongoose';
+import { PostCreateDTO, PostInputModel } from './post.models';
+
+export type PostDocument = HydratedDocument<Post>;
+
+export type PostModel = Model<PostDocument>;
+@Schema()
+export class Post {
+  @Prop({ required: true })
+  title: string;
+  @Prop({ required: true })
+  shortDescription: string;
+  @Prop({ required: true })
+  content: string;
+  @Prop({ required: true })
+  blogId: string;
+  @Prop({ required: true })
+  blogName: string;
+  @Prop({ required: true })
+  addedAt: string;
+  static create(
+    inputModel: PostInputModel,
+    blogId: string,
+    blogName: string,
+  ): PostCreateDTO {
+    const post: Post = new Post();
+    post.title = inputModel.title;
+    post.shortDescription = inputModel.shortDescription;
+    post.content = post.content;
+    post.blogId = inputModel.blogId || blogId;
+    post.blogName = blogName;
+    post.addedAt = new Date().toISOString();
+    return post;
+  }
+}
+
+export const PostSchema = SchemaFactory.createForClass(Post);
+
+export type LikeDBInfoDocument = HydratedDocument<LikeDBInfo>;
+@Schema()
+export class LikeDBInfo {
+  @Prop({ required: true })
+  status: string;
+  @Prop({ required: true })
+  userId: string;
+  @Prop({ required: true })
+  userLogin: string;
+  @Prop({ required: true })
+  postOrCommentId: string;
+  @Prop({ required: true })
+  createdAt: Date;
+}
