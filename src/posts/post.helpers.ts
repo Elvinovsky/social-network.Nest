72e81eeaf1986f7b116d77fd642d79f54260a5a7
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-09 21:14:34 +0500

    Update post.helpers.ts

--- src/posts/post.helpers.ts
+++ src/posts/post.helpers.ts
@@ -1,5 +1,8 @@
-import { PostDocument } from './post.schemas';
-import { LikeInfoView, PostViewDTO } from './post.models';
+import { Like, LikeModel, PostDocument } from './post.schemas';
+import { LikeDBInfo, LikeInfoView, PostViewDTO } from './post.models';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { LikeAndDisQuantity } from '../likes/like.helpers';
 
 export const postsMapping = async (
   array: Array<PostDocument>,
@@ -36,55 +39,61 @@ export const postsMapping = async (
     }),
   );
 };
-export const postMapping = async (
-  post: WithId<PostDBModel>,
-  userId?: string,
-): Promise<PostView> => {
-  const status = await likesQueryRepo.getLikeStatusCurrentUser(
-    post._id,
-    userId,
-  );
+@Injectable()
+export class PostMapping {
+  constructor(
+    @InjectModel(Like.name) private likeModel: LikeModel,
+    private readonly likeAndDisQuantity: LikeAndDisQuantity,
+  ) {}
+  async map(post: PostDocument, userId?: string): Promise<PostViewDTO> {
+    const status = await likesQueryRepo.getLikeStatusCurrentUser(
+      post._id,
+      userId,
+    );
 
-  const countsLikeAndDis = await likesOrDisCount(post._id);
+    const countsLikeAndDis = await this.likeAndDisQuantity.count(
+      post._id.toString(),
+    );
 
-  const likes: LikeDBInfo[] = await LikeModelClass.find({
-    postOrCommentId: post._id.toString(),
-    status: 'Like',
-  });
+    const likes: LikeDBInfo[] = await LikeModelClass.find({
+      postOrCommentId: post._id.toString(),
+      status: 'Like',
+    });
 
-  const lastLikes: LikeInfoView[] = await Promise.all(
-    likes
-      .sort(function (a, b) {
-        return a.createdAt < b.createdAt
-          ? -1
-          : a.createdAt > b.createdAt
-          ? 1
-          : 0;
-      })
-      .reverse()
-      .map(async (lastLikes) => {
-        return {
-          addedAt: lastLikes.createdAt.toISOString(),
-          userId: lastLikes.userId,
-          login: lastLikes.userLogin,
-        };
-      })
-      .slice(0, 3),
-  );
+    const lastLikes: LikeInfoView[] = await Promise.all(
+      likes
+        .sort(function (a, b) {
+          return a.createdAt < b.createdAt
+            ? -1
+            : a.createdAt > b.createdAt
+            ? 1
+            : 0;
+        })
+        .reverse()
+        .map(async (lastLikes) => {
+          return {
+            addedAt: lastLikes.createdAt.toISOString(),
+            userId: lastLikes.userId,
+            login: lastLikes.userLogin,
+          };
+        })
+        .slice(0, 3),
+    );
 
-  return {
-    id: post._id.toString(),
-    title: post.title,
-    shortDescription: post.shortDescription,
-    content: post.content,
-    blogId: post.blogId,
-    blogName: post.blogName,
-    createdAt: post.createdAt,
-    extendedLikesInfo: {
-      likesCount: countsLikeAndDis.likes,
-      dislikesCount: countsLikeAndDis.disLikes,
-      myStatus: status,
-      newestLikes: lastLikes,
-    },
-  };
-};
+    return {
+      id: post._id.toString(),
+      title: post.title,
+      shortDescription: post.shortDescription,
+      content: post.content,
+      blogId: post.blogId,
+      blogName: post.blogName,
+      createdAt: post.addedAt,
+      extendedLikesInfo: {
+        likesCount: countsLikeAndDis.likes,
+        dislikesCount: countsLikeAndDis.disLikes,
+        myStatus: status,
+        newestLikes: lastLikes,
+      },
+    };
+  }
+}
