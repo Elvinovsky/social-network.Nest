b5aa90654091d95046f55d1d39bec1baaf66e0e5
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-12 15:59:56 +0500

    Update delete.db.repository.ts "renamed field 5"

--- src/CLEAR.DB.TESTS/delete.db.controller.ts
+++ src/CLEAR.DB.TESTS/delete.db.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Delete } from '@nestjs/common';
 import { DeleteDbRepository } from './delete.db.repository';
 
 @Controller('testing')
-export class deleteDBController {
+export class DeleteDBController {
   constructor(private deleteRepo: DeleteDbRepository) {}
   @Delete('all-data')
   async deleteDB() {
