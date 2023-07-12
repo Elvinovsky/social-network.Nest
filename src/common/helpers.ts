ba81346815ea2aca4d5ab6187a4ffddda0105b24
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-12 15:01:16 +0500

    Update helpers.ts "typed"

--- src/common/helpers.ts
+++ src/common/helpers.ts
@@ -1,3 +1,3 @@
 import mongoose from 'mongoose';
 
-export const objectIdHelper = (id) => new mongoose.Types.ObjectId(id);
+export const objectIdHelper = (id: string) => new mongoose.Types.ObjectId(id);
