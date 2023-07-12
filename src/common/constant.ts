0cd0f26648148dcce271a7629ca8a8f7a66e6bd8
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-12 21:08:21 +0500

    Update constant.ts " fix "

--- src/common/constant.ts
+++ src/common/constant.ts
@@ -3,6 +3,6 @@ export enum SortDirection {
   Desc = 'Desc',
 }
 
-export const DEFAULT_PAGE_SortBy = 'createdAt';
+export const DEFAULT_PAGE_SortBy = 'addedAt';
 export const DEFAULT_PAGE_NUMBER = 1;
 export const DEFAULT_PAGE_SIZE = 10;
