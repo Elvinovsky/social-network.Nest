ac2c957c44108f35545d312017864974f46ba24d
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-12 16:15:00 +0500

    Update delete.db.controller.ts "change httpCode"

--- src/CLEAR.DB.TESTS/delete.db.controller.ts
+++ src/CLEAR.DB.TESTS/delete.db.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Delete } from '@nestjs/common';
+import { Controller, Delete, HttpCode, HttpStatus } from '@nestjs/common';
 import { DeleteDbRepository } from './delete.db.repository';
 
 @Controller('testing')
 export class DeleteDBController {
   constructor(private deleteRepo: DeleteDbRepository) {}
   @Delete('all-data')
+  @HttpCode(HttpStatus.NO_CONTENT)
   async deleteDB() {
     await this.deleteRepo.deleteDB();
   }
