d576a64e2ffd0ffa4375157e3e52c446f7ebf5ca
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-12 22:38:48 +0500

    Create posts.query.repo.ts " added class PostsQueryRepo and method getSortedPosts "

---
+++ src/posts/posts.query.repo.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@nestjs/common';
+import mongoose from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Post, PostDocument, PostModel } from './post.schemas';
+import { PaginatorType } from '../pagination/pagination.models';
+import { PostViewDTO } from './post.models';
+import {
+  getDirection,
+  getPageNumber,
+  getPageSize,
+  getSkip,
+  getSortBy,
+  pagesCountOfBlogs,
+} from '../pagination/pagination.helpers';
+import { DEFAULT_PAGE_SortBy } from '../common/constant';
+import { PostsMapping } from './post.helpers';
+
+@Injectable()
+export class PostsQueryRepo {
+  constructor(
+    @InjectModel(Post.name) private postModel: PostModel,
+    private postsMapping: PostsMapping,
+  ) {}
+
+  async getSortedPosts(
+    searchTitleTerm: string | undefined,
+    pageNumber?: number,
+    pageSize?: number,
+    sortBy?: string,
+    sortDirection?: string,
+    userId?: string,
+  ): Promise<PaginatorType<PostViewDTO[]>> {
+    const filter: mongoose.FilterQuery<PostDocument> = {};
+    if (searchTitleTerm) {
+      filter.title = { $regex: searchTitleTerm, $options: 'i' };
+    }
+
+    const calculateOfFiles = await this.postModel.countDocuments(filter);
+    const foundPosts: PostDocument[] = await this.postModel
+      .find(filter)
+      .sort({
+        [getSortBy(sortBy)]: getDirection(sortDirection),
+        [DEFAULT_PAGE_SortBy]: getDirection(sortDirection),
+      })
+      .skip(getSkip(getPageNumber(pageNumber), getPageSize(pageSize)))
+      .limit(getPageSize(pageSize));
+    return {
+      pagesCount: pagesCountOfBlogs(calculateOfFiles, pageSize),
+      page: getPageNumber(pageNumber),
+      pageSize: getPageSize(pageSize),
+      totalCount: calculateOfFiles,
+      items: await this.postsMapping.posts(foundPosts, userId),
+    };
+  }
+}
