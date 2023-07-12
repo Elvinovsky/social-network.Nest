2e747b54f9af9922669c60e0119072ed2bb0e2f2
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-12 20:38:12 +0500

    Update posts.repository.ts "added createPost method "

--- src/posts/posts.repository.ts
+++ src/posts/posts.repository.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { PostModel } from './post.schemas';
+import { Post, PostDocument, PostModel } from './post.schemas';
+import { PostCreateDTO } from './post.models';
+import { PostsMapping } from './post.helpers';
 
 @Injectable()
 export class PostsRepository {
-  constructor(@InjectModel('posts') private readonly postModel: PostModel) {}
+  constructor(
+    @InjectModel(Post.name) private readonly postModel: PostModel,
+    private readonly postsMapping: PostsMapping,
+  ) {}
+
+  async createPost(inputDTO: PostCreateDTO) {
+    try {
+      const post: PostDocument = new this.postModel(inputDTO);
+      await post.save();
+      return this.postsMapping.post(post);
+    } catch (e) {
+      const foundNewPost: PostDocument | null = await this.postModel.findOne({
+        addedAt: inputDTO.addedAt,
+      });
+
+      if (!foundNewPost) {
+        console.log(e, 'post not created');
+        return false;
+      }
+
+      console.log(e, 'post created');
+      return true;
+    }
+  }
 }
