e00950b7b32a9bfa0114b6b308b36cb02ac04b62
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-12 20:38:42 +0500

    Update posts.service.ts "added createPostByBLog method "

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,7 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { PostsRepository } from './posts.repository';
+import { BlogPostInputModel, PostCreateDTO, PostViewDTO } from './post.models';
+import { BlogsQueryRepo } from '../blogs/blogs.query.repo';
 
 @Injectable()
 export class PostsService {
-  constructor(private readonly postsRepository: PostsRepository) {}
+  constructor(
+    private readonly postsRepository: PostsRepository,
+    private blogsQueryRepo: BlogsQueryRepo,
+  ) {}
+  async createPostByBLog(
+    blogId: string,
+    inputModel: BlogPostInputModel,
+  ): Promise<PostViewDTO | boolean | null> {
+    const foundBlog = await this.blogsQueryRepo.getBlogById(blogId);
+    if (!foundBlog) {
+      return null;
+    }
+
+    const newPost: PostCreateDTO = {
+      title: inputModel.title,
+      shortDescription: inputModel.shortDescription,
+      content: inputModel.content,
+      blogId: foundBlog.id,
+      blogName: foundBlog.name,
+      addedAt: new Date().toISOString(),
+    };
+
+    return this.postsRepository.createPost(newPost);
+  }
 }
