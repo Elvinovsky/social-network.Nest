f5723c19c54aa7619d65a7aaa4d6bb4f9b235d50
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-14 22:29:57 +0500

    Update users.schema.ts "global change structure"

--- src/users/users.schema.ts
+++ src/users/users.schema.ts
@@ -1,25 +1,35 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
+import { EmailConfirmationModel } from './user.models';
 
 export type UserDocument = HydratedDocument<User>;
 
 @Schema()
 export class User {
   @Prop()
-  name: string;
+  login: string;
 
   @Prop()
-  childrenCount: number;
+  passwordHash: string;
 
-  setChildren(newChildrenCount: number) {
-    if (newChildrenCount < 0) {
+  @Prop()
+  email: string;
+
+  @Prop()
+  createdAt: string;
+
+  @Prop()
+  emailConfirmation: EmailConfirmationModel;
+
+  canBeConfirmed(expirationDate: Date) {
+    if (expirationDate < new Date()) {
       throw new Error('bad value');
     }
-    this.childrenCount = newChildrenCount;
+    this.emailConfirmation.expirationDate = expirationDate;
   }
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
 UserSchema.methods = {
-  setChildren: User.prototype.setChildren,
+  setChildren: User.prototype.canBeConfirmed,
 };
