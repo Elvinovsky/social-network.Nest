842716f7c59f0cdfb9bcb372970fb0e0214bba2d
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-14 22:29:22 +0500

    Update users.controller.ts "change getUsers  "

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,33 +1,47 @@
 import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { UserInputModel } from './user.models';
+import {
+  QueryInputModel,
+  SearchEmailTerm,
+  SearchLoginTerm,
+} from '../pagination/pagination.models';
+import { UsersQueryRepository } from './users.query.repo';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly usersQueryRepo: UsersQueryRepository,
+  ) {}
   @Get()
-  async getUsers() {
-    return this.usersService.getUsers();
+  async getUsers(
+    @Query() query: QueryInputModel & SearchEmailTerm & SearchLoginTerm,
+  ) {
+    return this.usersQueryRepo.getSortedUsers(
+      query.searchEmailTerm,
+      query.searchLoginTerm,
+      Number(query.pageNumber),
+      Number(query.pageSize),
+      query.sortBy,
+      query.sortDirection,
+    );
   }
   @Get(':id')
   async getUser(@Param('id') userId: string) {
     return this.usersService.getUser(userId.toString());
   }
   @Post()
-  async createUsers(@Body() inputModel: CreateUserInputType) {
+  async createUsers(@Body() inputModel: UserInputModel) {
     return this.usersService.createUser(inputModel);
   }
   @Put(':id')
   async updateUser(
     @Param('id') userId: number,
-    @Body() inputModel: CreateUserInputType,
+    @Body() inputModel: UserInputModel,
   ) {
     const user = await this.usersService.getUser(userId.toString());
-    if (user) user.setChildren(inputModel.childrenCount);
+    if (user) user.canBeConfirmed(user.emailConfirmation.expirationDate);
     return this.usersService.updateUser(user);
   }
 }
-
-export type CreateUserInputType = {
-  name: string;
-  childrenCount: number;
-};
