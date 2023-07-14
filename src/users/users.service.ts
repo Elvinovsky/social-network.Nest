6fe69b823ce155316cfa33e5bf85fb9f68748a98
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-14 23:11:07 +0500

    Update users.service.ts "changed logic structure 'createUser' and added '_generateHash' method"

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,22 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
-import { CreateUserInputType } from './users.controller';
 import { UserDocument } from './users.schema';
-
+import { UserInputModel, UserViewDTO } from './user.models';
+import bcrypt from 'bcrypt';
 @Injectable()
 export class UsersService {
   constructor(protected usersRepository: UsersRepository) {}
-  async getUsers() {
-    return this.usersRepository.getUsers();
-  }
   async getUser(userId: string): Promise<UserDocument | null> {
     return this.usersRepository.getUser(userId);
   }
-  async createUser(inputModel: CreateUserInputType) {
-    const user = await this.usersRepository.createUser(inputModel);
-    return this.usersRepository.save(user);
+  async createUser(inputModel: UserInputModel): Promise<UserViewDTO> {
+    const hash = await this._generateHash(inputModel.password);
+
+    return await this.usersRepository.createUser(inputModel, hash);
   }
+
   async updateUser(user) {
     return this.usersRepository.updateUser(user);
+  }
+  async _generateHash(password: string): Promise<string> {
+    return await bcrypt.hash(password, 7);
   }
 }
