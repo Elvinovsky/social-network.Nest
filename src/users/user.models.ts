916497835cfb96117e99f32fe92d190fde785538
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-16 22:11:56 +0500

    Update user.models.ts "change type UserInputModel "

--- src/users/user.models.ts
+++ src/users/user.models.ts
@@ -1,17 +1,26 @@
-export type UserInputModel = {
+import { IsNotEmpty, Length, Matches } from 'class-validator';
+
+export class UserInputModel {
+  @IsNotEmpty()
+  @Length(3, 10)
+  @Matches(/^[a-zA-Z0-9_-]*$/)
   login: string;
   /**
-   * maxLength: 10, minLength: 3, pattern: ^[a-zA-Z0-9_-]*$
+   * maxLength: 10, minLength: 3, pattern: c
    */
+  @IsNotEmpty()
+  @Length(6, 20)
   password: string;
   /**
    *   maxLength: 20,  minLength: 6
    */
+  @IsNotEmpty()
+  @Matches(/^[\w-]+@([\w-]+\.)+[\w-]{2,4}$/)
   email: string;
   /**
    * pattern: ^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$
    */
-};
+}
 
 export class UserCreateDTO {
   login: string;
