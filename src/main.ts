2cd223282962e37ebb8e1c5389362ff6dddfc934
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-17 23:41:44 +0500

    Update main.ts "change optional useGlobalPipes "

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import {
+  BadRequestException,
+  ValidationError,
+  ValidationPipe,
+} from '@nestjs/common';
 import { HttpExceptionFilter } from './http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      stopAtFirstError: true,
+      exceptionFactory: (errors: ValidationError[]) => {
+        throw new BadRequestException(
+          errors.map((e: ValidationError) => {
+            return {
+              field: e.property,
+              message: 'invalid',
+            };
+          }),
+        );
+      },
+    }),
+  );
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
