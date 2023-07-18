31e61b96cf039e897c55eeaa42ab4ebded45d5f7
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-19 04:29:07 +0500

    Update blogs.repository.ts "refactored"

--- src/blogs/blogs.repository.ts
+++ src/blogs/blogs.repository.ts
@@ -5,11 +5,14 @@ import { Blog, BlogModel } from './blog.schemas';
 import { blogMapping } from './blog.helpers';
 import { objectIdHelper } from '../common/helpers';
 
+// Репозиторий блогов, который используется для выполнения операций CRUD
 // принимает 'BlogInputModel' трансформирует его для заданного хранения схемы 'BlogCreateDTO', вся логика изменения данных для входа и выхода производится в репозитории
 @Injectable()
 export class BlogsRepository {
   constructor(@InjectModel(Blog.name) private blogModel: BlogModel) {}
-  //поиск блога по ID.
+
+  // Находит блог по заданному ID
+  // Возвращает BlogViewDTO или null, если блог не найден
   async findBlogById(id: string): Promise<BlogViewDTO | null> {
     try {
       const blog = await this.blogModel.findById(objectIdHelper(id));
@@ -22,17 +25,23 @@ export class BlogsRepository {
       throw new HttpException('failed', HttpStatus.EXPECTATION_FAILED);
     }
   }
+
+  // Добавляет новый блог на основе входной модели BlogInputModel
+  // Возвращает BlogViewDTO созданного блога
   async addNewBlog(inputModel: BlogInputModel): Promise<BlogViewDTO> {
     try {
       const createBlog: BlogCreateDTO = Blog.createBlog(inputModel);
-      const blogDoc = await new this.blogModel(createBlog);
-      await blogDoc.save();
-      return blogMapping(blogDoc);
+      const createdBlog = await new this.blogModel(createBlog);
+      await createdBlog.save();
+      return blogMapping(createdBlog);
     } catch (e) {
       console.log(e);
       throw new HttpException('failed', HttpStatus.EXPECTATION_FAILED);
     }
   }
+
+  // Обновляет блог с заданным ID на основе входной модели BlogInputModel
+  // Возвращает true, если блог был успешно обновлен, false в противном случае
   async updateBlogById(
     id: string,
     inputModel: BlogInputModel,
@@ -55,7 +64,8 @@ export class BlogsRepository {
     }
   }
 
-  //поиск блога по ID для удаления.
+  // Удаляет блог с заданным ID
+  // Возвращает удаленный документ или null, если блог не найден
   async deleteBlogById(id: string): Promise<Document | null> {
     try {
       return await this.blogModel.findByIdAndDelete({
