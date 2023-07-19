941448e9a04fda4402d180e94a52c154542584e6
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-19 05:13:36 +0500

    Create constant.ts "added Status enum"

--- src/common/constant.ts
+++ src/common/constant.ts
@@ -3,6 +3,12 @@ export enum SortDirection {
   Desc = -1,
 }
 
+export enum Status {
+  None = 'None',
+  Like = 'Like',
+  Dislike = 'Dislike',
+}
+
 export const DEFAULT_PAGE_SortBy = 'addedAt';
 export const DEFAULT_PAGE_NUMBER = 1;
 export const DEFAULT_PAGE_SIZE = 10;
