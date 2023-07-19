b3bb5682680c1ae80b09f9a5a160b45e343d40d3
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-19 05:17:51 +0500

    Create like.helpers.ts "postponed enum Status"

--- src/likes/like.helpers.ts
+++ src/likes/like.helpers.ts
@@ -1,12 +1,8 @@
-import { Like, LikeModel } from '../posts/post.schemas';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { Like, LikeModel } from './like.schemas';
+import { Status } from '../common/constant';
 
-export enum Status {
-  None = 'None',
-  Like = 'Like',
-  Dislike = 'Dislike',
-}
 @Injectable()
 export class LikeAndDisQuantity {
   constructor(@InjectModel(Like.name) private likeModel: LikeModel) {}
