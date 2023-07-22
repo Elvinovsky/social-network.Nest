7831b001f9d513a97428a03c1932ca5a192fbbef
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-22 23:32:15 +0500

    Create posts.service.ts 'fixed logic for SOLID concept '

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,23 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { PostsRepository } from './posts.repository';
 import { BlogPostInputModel, PostInputModel, PostViewDTO } from './post.models';
-import { BlogsQueryRepo } from '../blogs/blogs.query.repo';
-import { BlogViewDTO } from '../blogs/blog.models';
+import { BlogsService } from '../blogs/blogs.service';
+import { BlogDocument } from '../blogs/blog.schemas';
 
 @Injectable()
 export class PostsService {
   constructor(
     private readonly postsRepository: PostsRepository,
-    private readonly blogsQueryRepo: BlogsQueryRepo,
+    private readonly blogService: BlogsService,
   ) {}
 
   async createPostByBLog(
     id: string,
-    inputModel: BlogPostInputModel, // todo repository
+    inputModel: BlogPostInputModel,
   ): Promise<PostViewDTO | null> {
-    const foundBlog: BlogViewDTO | null = await this.blogsQueryRepo.getBlogById(
-      id,
-    );
+    const foundBlog: BlogDocument | null = await this.blogService.findById(id);
     if (!foundBlog) {
       return null;
     }
@@ -26,7 +24,7 @@ export class PostsService {
   }
 
   async createPost(inputModel: PostInputModel): Promise<PostViewDTO | null> {
-    const foundBlog: BlogViewDTO | null = await this.blogsQueryRepo.getBlogById(
+    const foundBlog: BlogDocument | null = await this.blogService.findById(
       inputModel.blogId,
     );
     if (!foundBlog) {
@@ -38,9 +36,9 @@ export class PostsService {
 
   async updatePost(
     id: string,
-    inputModel: PostInputModel, // todo repository!!
+    inputModel: PostInputModel,
   ): Promise<boolean | null> {
-    const foundBlog: BlogViewDTO | null = await this.blogsQueryRepo.getBlogById(
+    const foundBlog: BlogDocument | null = await this.blogService.findById(
       inputModel.blogId,
     );
 
