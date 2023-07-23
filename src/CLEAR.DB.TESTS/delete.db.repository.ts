a4297cc221773d0812350bc3036f1f37059be806
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-23 15:16:13 +0500

    Update delete.db.repository.ts 'changing dependency injection'

--- src/CLEAR.DB.TESTS/delete.db.repository.ts
+++ src/CLEAR.DB.TESTS/delete.db.repository.ts
@@ -5,6 +5,7 @@ import { Post, PostModel } from '../posts/post.schemas';
 import { User } from '../users/users.schema';
 import { Model } from 'mongoose';
 import { Like, LikeModel } from '../likes/like.schemas';
+import { Comment, CommentModel } from '../comments/comment.schemas';
 
 @Injectable()
 export class DeleteDbRepository {
@@ -12,6 +13,7 @@ export class DeleteDbRepository {
     @InjectModel(User.name) private userModel: Model<User>,
     @InjectModel(Blog.name) private blogModel: BlogModel,
     @InjectModel(Post.name) private postModel: PostModel,
+    @InjectModel(Comment.name) private commentModel: CommentModel,
     @InjectModel(Like.name) private likeModel: LikeModel,
   ) {}
   async deleteDB() {
@@ -19,5 +21,6 @@ export class DeleteDbRepository {
     await this.likeModel.deleteMany();
     await this.userModel.deleteMany();
     await this.postModel.deleteMany();
+    await this.commentModel.deleteMany();
   }
 }
