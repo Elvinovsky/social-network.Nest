f55b2ae51bb1f03cc6b28de6ddf5080e48a53c2f
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-24 23:53:19 +0500

    Update users.service.ts "added method 'createUserRegistration'"

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,18 +1,50 @@
 import { Injectable } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
 import { UserDocument } from './users.schema';
-import { UserInputModel, UserViewDTO } from './user.models';
+import { UserCreateDTO, UserInputModel, UserViewDTO } from './user.models';
 import bcrypt from 'bcrypt';
+import { add } from 'date-fns';
+import { v4 as uuidv4 } from 'uuid';
 @Injectable()
 export class UsersService {
   constructor(protected usersRepository: UsersRepository) {}
   async getUser(userId: string): Promise<UserDocument | null> {
     return this.usersRepository.getUser(userId);
   }
-  async createUser(inputModel: UserInputModel): Promise<UserViewDTO> {
-    const hash = await this._generateHash(inputModel.password);
 
-    return await this.usersRepository.createUser(inputModel, hash);
+  async createUserForSA(inputModel: UserInputModel): Promise<UserViewDTO> {
+    const hash = await this._generateHash(inputModel.password); //todo logic registr => auth?
+    const newUser: UserCreateDTO = {
+      login: inputModel.login,
+      passwordHash: hash,
+      email: inputModel.email,
+      addedAt: new Date().toISOString(),
+      emailConfirmation: {
+        confirmationCode: 'not required',
+        expirationDate: 'not required',
+        isConfirmed: true,
+      },
+    };
+
+    return await this.usersRepository.createUser(newUser);
+  }
+
+  async createUserRegistration(inputModel: UserInputModel, hash: string) {
+    const newUser: UserCreateDTO = {
+      login: inputModel.login,
+      passwordHash: hash,
+      email: inputModel.email,
+      addedAt: new Date().toISOString(),
+      emailConfirmation: {
+        confirmationCode: uuidv4(),
+        expirationDate: add(new Date(), {
+          hours: 1,
+          minutes: 10,
+        }),
+        isConfirmed: false,
+      },
+    };
+    return this.usersRepository.createUser(newUser);
   }
 
   async updateUser(userId: string, inputModel: UserInputModel) {
