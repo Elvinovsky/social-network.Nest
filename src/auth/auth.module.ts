bfdff76b6bcb3865e6265a11df02d652bc041b09
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-26 23:41:09 +0500

    Create auth.module.ts

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,12 @@
+// import { PassportModule } from '@nestjs/passport';
+// import { BasicStrategy } from './strategies/basic.strategy';
+// import { Module } from '@nestjs/common';
+// import { AuthController } from './auth.controller';
+//
+// @Module({
+//   imports: [PassportModule],
+//   providers: [BasicStrategy],
+//   controllers: [AuthController],
+//
+// })
+// export class AuthModule {}
