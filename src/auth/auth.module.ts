beb7672d4239d5ae6968f01254a0a21a1eeb2064
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-29 22:50:37 +0500

    Update auth.module.ts "added module for Auth registry AuthRepository,                                                           AuthService,                                                           UsersService,                                                           BasicStrategy,                                                           BasicAuthGuard,                                                           LocalAuthGuard,                                                           LocalStrategy,                                                           UsersRepository,
                                                              JwtService,
                                                              EmailService,"

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,47 @@
-// import { PassportModule } from '@nestjs/passport';
-// import { BasicStrategy } from './strategies/basic.strategy';
-// import { Module } from '@nestjs/common';
-// import { AuthController } from './auth.controller';
-//
-// @Module({
-//   imports: [PassportModule],
-//   providers: [BasicStrategy],
-//   controllers: [AuthController],
-//
-// })
-// export class AuthModule {}
+import { PassportModule } from '@nestjs/passport';
+import { BasicStrategy } from './strategies/basic.strategy';
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { jwtConstants } from './auth.constants';
+import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
+import { LocalStrategy } from './strategies/local.strategy';
+import { AuthRepository } from './auth.repository';
+
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from '../users/users.schema';
+import { EmailService } from '../email/email.service';
+import { UsersRepository } from '../users/users.repository';
+import { BasicAuthGuard } from './guards/basic-auth.guard';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { UsersModule } from '../users/users.module';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secretAccess,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [
+    AuthRepository,
+    AuthService,
+    UsersService,
+    BasicStrategy,
+    BasicAuthGuard,
+    LocalAuthGuard,
+    LocalStrategy,
+    UsersRepository,
+
+    JwtService,
+    EmailService,
+  ],
+  controllers: [AuthController],
+  exports: [AuthService],
+})
+export class AuthModule {}
