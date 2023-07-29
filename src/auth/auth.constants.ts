ee12d9e887f608768a64e644c32417187603f726
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-29 22:51:13 +0500

    Update auth.constants.ts "added constants for jwt secret "

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -2,3 +2,8 @@ export const basicConstants = {
   userName: process.env.BASIC_USER_NAME || 'admin',
   password: process.env.BASIC_PASS || '123',
 };
+
+export const jwtConstants = {
+  secretAccess: process.env.ACCESS_JWT_SECRET_KEY,
+  secretRefresh: process.env.REFRESH_JWT_SECRET_KEY,
+};
