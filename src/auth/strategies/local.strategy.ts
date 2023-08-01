68498a0495c6bd7fac0e7ce95cb41f42a689d33f
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-01 21:32:09 +0500

    Update local.strategy.ts

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -1,7 +1,7 @@
-import { Strategy } from 'passport';
 import { PassportStrategy } from '@nestjs/passport';
 import { AuthService } from '../auth.service';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Strategy } from 'passport-local';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
