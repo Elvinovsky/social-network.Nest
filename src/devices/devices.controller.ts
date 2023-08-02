15720423713aec55bf8b11efe5dc279789701c71
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-02 23:02:24 +0500

    Update devices.controller.ts "commented out endpoints"

--- src/devices/devices.controller.ts
+++ src/devices/devices.controller.ts
@@ -1,44 +1,45 @@
-import {
-  Controller,
-  Delete,
-  ForbiddenException,
-  Get,
-  NotFoundException,
-  UseGuards,
-} from '@nestjs/common';
-import { DevicesService } from './devices.service';
-import { JwtRefreshGuard } from '../auth/guards/jwt-refresh.guard';
-@UseGuards(JwtRefreshGuard)
-@Controller('devices')
-export class DevicesController {
-  constructor(private devicesService: DevicesService) {}
-  @Get()
-  async getDevices() {
-    // const userId = req.userId.toString()
-    const devicesSessionsByUser =
-      await this.devicesService.findDevicesSessionsByUserId('userId');
-  }
-
-  @Delete()
-  async deleteDevices() {
-    const userId = req.userId.toString();
-    await this.devicesService.logoutDevicesSessionsByUser(req.issuedAt, userId);
-    res.sendStatus(204);
-    return;
-  }
-  @Delete()
-  async deleteDeviceById(req: Request, res: Response) {
-    const deviceId = req.params.deviceId;
-    const userId = req.userId.toString();
-    const logoutDeviceSession: boolean | null =
-      await this.devicesService.logoutDeviceSessionByDeviceId(deviceId, userId);
-
-    if (logoutDeviceSession === null) {
-      throw new NotFoundException();
-    }
-
-    if (!logoutDeviceSession) {
-      throw new ForbiddenException();
-    }
-  }
-}
+// import {
+//   Controller,
+//   Delete,
+//   ForbiddenException,
+//   Get,
+//   NotFoundException,
+//   UseGuards,
+// } from '@nestjs/common';
+// import { DevicesService } from './devices.service';
+// import { JwtRefreshGuard } from '../auth/guards/jwt-refresh.guard';
+// @UseGuards(JwtRefreshGuard)
+// @Controller('devices')
+// export class DevicesController {
+//   constructor(private devicesService: DevicesService) {}
+//   @Get()
+//   async getDevices() {
+//     // const userId = req.userId.toString()
+//     const devicesSessionsByUser =
+//       await this.devicesService.findDevicesSessionsByUserId('userId');
+//   }
+//
+//   @UseGuards(JwtRefreshGuard)
+//   @Delete()
+//   async deleteDevices() {
+//     const userId = req.userId.toString();
+//     await this.devicesService.logoutDevicesSessionsByUser(req.issuedAt, userId);
+//     res.sendStatus(204);
+//     return;
+//   }
+//   @Delete()
+//   async deleteDeviceById(req: Request, res: Response) {
+//     const deviceId = req.params.deviceId;
+//     const userId = req.userId.toString();
+//     const logoutDeviceSession: boolean | null =
+//       await this.devicesService.logoutDeviceSessionByDeviceId(deviceId, userId);
+//
+//     if (logoutDeviceSession === null) {
+//       throw new NotFoundException();
+//     }
+//
+//     if (!logoutDeviceSession) {
+//       throw new ForbiddenException();
+//     }
+//   }
+// }
