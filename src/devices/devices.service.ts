8f50c4680cc1047943080807061f93bbf3a64f0f
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-02 21:18:00 +0500

    Create devices.service.ts "added class business layer to devices"

---
+++ src/devices/devices.service.ts
@@ -0,0 +1,61 @@
+import add from 'date-fns/add';
+import { Injectable } from '@nestjs/common';
+import { DevicesRepository } from './devices.repository';
+import { SessionCreateDTO } from './device.models';
+import mongoose from 'mongoose';
+@Injectable()
+export class DevicesService {
+  constructor(protected devicesRepository: DevicesRepository) {}
+  async findDevicesSessionsByUserId(userId: string) {
+    return this.devicesRepository.findDevicesSessionsByUserId(userId);
+  }
+
+  async createDeviceSession(
+    userId: mongoose.Types.ObjectId,
+    deviceId: string,
+    issuedAt: number,
+    ip: string | null,
+    deviceName?: string,
+  ) {
+    const createDeviceSession: SessionCreateDTO = {
+      deviceId: deviceId,
+      issuedAt: issuedAt,
+      userId: userId.toString(),
+      ip: ip || null,
+      title: deviceName || null,
+      lastActiveDate: new Date().toISOString(),
+      expirationDate: add(new Date(), {
+        seconds: 20,
+        //minutes:20
+      }),
+    };
+    return await this.devicesRepository.addDeviceSession(createDeviceSession);
+  }
+
+  async updateIATByDeviceSession(newIssuedAt: number, issuedAt: number) {
+    return await this.devicesRepository.updateDeviceSession(
+      newIssuedAt,
+      issuedAt,
+    );
+  }
+
+  async logoutDeviceSessionByDeviceId(deviceId: string, userId: string) {
+    const findDeviceSessionById =
+      await this.devicesRepository.findDeviceIdAmongSessions(deviceId);
+    if (!findDeviceSessionById) {
+      return null;
+    }
+    return await this.devicesRepository.deleteDeviceSessionSpecified(
+      deviceId,
+      userId,
+    );
+  }
+
+  async logoutDevicesSessionsByUser(issuedAt: number, userId: string) {
+    const result = await this.devicesRepository.deleteDevicesSessionsByUser(
+      issuedAt,
+      userId,
+    );
+    return result;
+  }
+}
