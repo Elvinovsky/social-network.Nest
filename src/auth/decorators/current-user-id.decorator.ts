26577275b0572a65cd42a3835690edf48f0fb4c4
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-03 22:01:25 +0500

    Update current-user-id.decorator.ts "fix"

--- src/auth/decorators/current-user-id.decorator.ts
+++ src/auth/decorators/current-user-id.decorator.ts
@@ -1,10 +1,14 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Request } from 'express';
+import { UserViewDTO } from '../../users/user.models';
 
 export const CurrentUserId = createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
     const ctx = context.switchToHttp();
-    const request = ctx.getRequest<Request>();
-    return request.user;
+    const request: Request = ctx.getRequest();
+
+    const user = request.user as UserViewDTO;
+
+    return user.id;
   },
 );
