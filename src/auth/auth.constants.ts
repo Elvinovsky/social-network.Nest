79a625c63baafd751b607cf894e0621052fcbe46
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-04 11:24:48 +0500

    Update auth.constants.ts "fix"

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -1,11 +1,13 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 export const basicConstants = {
   userName: process.env.BASIC_USER_NAME || 'admin',
   password: process.env.BASIC_PASS || '123',
 };
-
+//todo как убрать undefined;
 export const jwtConstants = {
-  secretAccess: process.env.ACCESS_JWT_SECRET_KEY,
-  secretRefresh: process.env.REFRESH_JWT_SECRET_KEY,
+  secretAccess: process.env.ACCESS_JWT_SECRET_KEY || 'string',
+  secretRefresh: process.env.REFRESH_JWT_SECRET_KEY || 'string',
   accessTokenExpirationTime: '60s',
   refreshTokenExpirationTime: '60s',
 };
