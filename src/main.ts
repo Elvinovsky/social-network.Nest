50b673e1fc1e74af7d2f8a4980e7872af6658c42
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-04 14:34:36 +0500

    Update main.ts 'use cookieParser'

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
 import {
   BadRequestException,
   ValidationError,
@@ -12,6 +13,7 @@ import {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
