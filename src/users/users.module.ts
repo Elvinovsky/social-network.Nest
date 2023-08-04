7156a40380393be65e0249ffed445c69807b2458
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-04 16:10:27 +0500

    Update users.module.ts ' add forwardRef '

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,12 +3,14 @@ import { UsersRepository } from './users.repository';
 import { UsersQueryRepository } from './users.query.repo';
 import { User, UserSchema } from './users.schema';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    forwardRef(() => AuthModule),
   ],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository, UsersQueryRepository],
