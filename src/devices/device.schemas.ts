d88b497a942c7cb8be154aa224459cfd10ce451c
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-04 14:09:47 +0500

    Update device.schemas.ts "fix "

--- src/devices/device.schemas.ts
+++ src/devices/device.schemas.ts
@@ -13,9 +13,9 @@ export class Device {
   @Prop({ required: true })
   userId: string;
   @Prop({ required: true })
-  ip: string | null;
+  ip: string;
   @Prop({ required: true })
-  title: string | null;
+  title: string;
   @Prop({ required: true })
   lastActiveDate: string;
   @Prop({ required: true })
