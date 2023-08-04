0f0f28e02163dc6cb4bfecbfcac2c2dff2d4026a
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-04 14:12:27 +0500

    Update helpers.ts 'added "refreshCookieOptions"'

--- src/common/helpers.ts
+++ src/common/helpers.ts
@@ -1,7 +1,15 @@
 import mongoose from 'mongoose';
+import { CookieOptions } from 'express';
+
 export const objectIdHelper = (id: string) => {
   if (mongoose.Types.ObjectId.isValid(id)) {
     return new mongoose.Types.ObjectId(id);
   }
   return null;
+};
+
+export const refreshCookieOptions: CookieOptions = {
+  httpOnly: true,
+  sameSite: 'none',
+  secure: true,
 };
