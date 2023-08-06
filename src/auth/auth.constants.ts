3087d5a49d2c6e5eb4c1ad3a5e1167c46ead3dfc
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-06 14:53:25 +0500

    Update auth.constants.ts

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -4,6 +4,7 @@ export const basicConstants = {
   userName: process.env.BASIC_USER_NAME || 'admin',
   password: process.env.BASIC_PASS || 'qwerty',
 };
+
 //todo как убрать undefined;
 export const jwtConstants = {
   secretAccess: process.env.ACCESS_JWT_SECRET_KEY || 'string',
