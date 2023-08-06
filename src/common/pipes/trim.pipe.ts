321ab4ab41ddd0fbc44ff66626c2dbca0d7323f4
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-06 22:19:29 +0500

    Create trim.pipe.ts ' added validation to spaces'

---
+++ src/common/pipes/trim.pipe.ts
@@ -0,0 +1,14 @@
+import { PipeTransform, Injectable } from '@nestjs/common';
+@Injectable()
+export class TrimPipe implements PipeTransform {
+  transform(value: any) {
+    if (typeof value !== 'object') {
+      return value;
+    }
+    Object.keys(value).forEach((el) => {
+      return (value[el] =
+        typeof value[el] === 'string' ? value[el].trim() : value[el]);
+    });
+    return value;
+  }
+}
