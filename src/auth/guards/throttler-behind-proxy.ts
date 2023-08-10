e3a77c979ec47a31a6c5a420b860b795ae68f722
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-10 21:22:57 +0500

    Update throttler-behind-proxy.ts "realize"

--- src/auth/guards/throttler-behind-proxy.ts
+++ src/auth/guards/throttler-behind-proxy.ts
@@ -1,10 +1,34 @@
 // throttler-behind-proxy.guard.ts
-import { ThrottlerGuard } from '@nestjs/throttler';
-import { Injectable } from '@nestjs/common';
+import { ThrottlerException, ThrottlerGuard } from '@nestjs/throttler';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ThrottlerBehindProxyGuard extends ThrottlerGuard {
   protected getTracker(req: Record<string, any>): string {
     return req.ips.length ? req.ips[0] : req.ip;
+  }
+}
+
+//todo realize
+@Injectable()
+export class WsThrottlerGuard extends ThrottlerGuard {
+  async handleRequest(
+    context: ExecutionContext,
+    limit: number,
+    ttl: number,
+  ): Promise<boolean> {
+    const client = context.switchToWs().getClient();
+    const ip =
+      client._socket.remoteAddress || client.ips.length
+        ? client.ips[0]
+        : client.ip;
+    const key = this.generateKey(context, ip);
+    const { totalHits } = await this.storageService.increment(key, ttl);
+
+    if (totalHits > limit) {
+      throw new ThrottlerException();
+    }
+
+    return true;
   }
 }
