d14f7a8733ba1f50111982c23586323d8cef9ff9
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-11 20:47:21 +0500

    Create optional-bearer.guard.ts

---
+++ src/auth/guards/optional-bearer.guard.ts
@@ -0,0 +1,29 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { jwtConstants } from '../auth.constants';
+
+@Injectable()
+export class optionalUserAuth implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const accessToken = this.extractTokenFromHeaders(request);
+
+    if (!accessToken) {
+      request.userId = null;
+    } else {
+      const payload = await this.jwtService.verifyAsync(accessToken, {
+        secret: jwtConstants.secretAccess,
+      });
+      request.userId = payload.userId;
+    }
+    return true;
+  }
+
+  private extractTokenFromHeaders(request: Request): string | null {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : null;
+  }
+}
