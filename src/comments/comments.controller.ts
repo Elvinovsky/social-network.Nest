d4f50e5e75b0dd5b455c622ba8b6c5a11a492e63
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-11 23:14:41 +0500

    Update comment.schemas.ts

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
 import { CommentsService } from './comments.service';
+import { OptionalBearerGuard } from '../auth/guards/optional-bearer.guard';
 
 @Controller('/comments')
 export class CommentsController {
   constructor(private commentService: CommentsService) {}
   @Get(':commentId')
+  @UseGuards(OptionalBearerGuard)
   async getComment(@Param('commentId') commentId: string, user?) {
     const comment = await this.commentService.getComment(commentId, user?.id);
     if (!comment) {
