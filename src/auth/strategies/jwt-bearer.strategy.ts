9686ab075665145931c8d56f312b40cd6806d1a9
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-11 20:46:44 +0500

    Update jwt-bearer.strategy.ts "transfer CheckUserIdGuard"

--- src/auth/strategies/jwt-bearer.strategy.ts
+++ src/auth/strategies/jwt-bearer.strategy.ts
@@ -11,37 +11,6 @@ export class JwtBearerStrategy extends PassportStrategy(Strategy, 'bearer') {
     });
   }
   async validate(payload: any) {
-    return {
-      userId: payload.userId,
-    };
-  }
-}
-
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { Request } from 'express';
-
-@Injectable()
-export class CheckUserIdGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
-
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
-    const accessToken = this.extractTokenFromHeaders(request);
-
-    if (!accessToken) {
-      request.userId = null;
-    } else {
-      const payload = await this.jwtService.verifyAsync(accessToken, {
-        secret: jwtConstants.secretAccess,
-      });
-      request.userId = payload.userId;
-    }
-    return true;
-  }
-
-  private extractTokenFromHeaders(request: Request): string | null {
-    const [type, token] = request.headers.authorization?.split(' ') ?? [];
-    return type === 'Bearer' ? token : null;
+    return payload.userId;
   }
 }
