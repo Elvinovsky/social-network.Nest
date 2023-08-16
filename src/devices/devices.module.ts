f594175b0535534501c4ba98ef285bd096d3ae2c
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-16 18:31:46 +0500

    Update devices.module.ts 'registration AuthModule'

--- src/devices/devices.module.ts
+++ src/devices/devices.module.ts
@@ -1,13 +1,17 @@
 import { MongooseModule } from '@nestjs/mongoose';
 import { Device, DeviceSchema } from './device.schemas';
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { DevicesController } from './devices.controller';
 import { DevicesService } from './devices.service';
 import { DevicesRepository } from './devices.repository';
+import { AuthModule } from '../auth/auth.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Device.name, schema: DeviceSchema }]),
+    forwardRef(() => AuthModule),
+    UsersModule,
   ],
   controllers: [DevicesController],
   providers: [DevicesService, DevicesRepository],
