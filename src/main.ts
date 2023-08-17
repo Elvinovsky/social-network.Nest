4c919af3c07f8145064e3e59cbe6b7f5abcfd25b
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-17 22:10:44 +0500

    Create app-settings.ts ' added a function with settings for launching the application '

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cookieParser from 'cookie-parser';
-import {
-  BadRequestException,
-  ValidationError,
-  ValidationPipe,
-} from '@nestjs/common';
-import {
-  HttpExceptionFilter,
-  ErrorExceptionFilter,
-} from './http-exception.filter';
-import { TrimPipe } from './common/pipes/trim.pipe';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { useContainer } from 'class-validator';
+import { appSettings } from './settings/app-settings';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.use(cookieParser());
-  useContainer(app.select(AppModule), { fallbackOnErrors: true });
-  app.useGlobalPipes(
-    new TrimPipe(),
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-      stopAtFirstError: false,
-      exceptionFactory: (errors: ValidationError[]) => {
-        throw new BadRequestException(
-          errors.map((e: ValidationError) => {
-            return {
-              field: e.property,
-              message: e.property + ' invalid', // todo realize constrains
-            };
-          }),
-        );
-      },
-    }),
-  );
-  app.useGlobalFilters(new ErrorExceptionFilter(), new HttpExceptionFilter());
-  const config = new DocumentBuilder()
-    .setTitle('Social-network')
-    .setDescription('')
-    .setVersion('1.0')
-    .addTag('Social-network')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  await appSettings(app);
   await app.listen(3000);
 }
 
---
+++ src/settings/app-settings.ts
@@ -0,0 +1,55 @@
+import {
+  BadRequestException,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+import { useContainer } from 'class-validator';
+import { AppModule } from '../app.module';
+import {
+  ErrorExceptionFilter,
+  HttpExceptionFilter,
+} from '../http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export const appSettings = (app: INestApplication) => {
+  app.use(cookieParser());
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+  //todo - не получается перенести пайп в отдельный файл
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      stopAtFirstError: true,
+      exceptionFactory: (errors) => {
+        const errorsForResponse: any = [];
+
+        errors.forEach((err) => {
+          const keys = Object.keys(err.constraints || {});
+          keys.forEach((key) => {
+            if (err.constraints) {
+              errorsForResponse.push({
+                message: err.constraints[key],
+                field: err.property,
+              });
+            }
+          });
+        });
+
+        throw new BadRequestException(errorsForResponse);
+      },
+    }),
+  );
+
+  app.enableCors();
+  app.useGlobalFilters(new ErrorExceptionFilter(), new HttpExceptionFilter());
+  const config = new DocumentBuilder()
+    .setTitle('Social-network')
+    .setDescription('')
+    .setVersion('1.0')
+    .addTag('Social-network')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+};
