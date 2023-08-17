b6a24c3901094d019616a362d51e66312b14cfbf
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-17 09:31:53 +0500

    Update trim.pipe.ts 'add pipe for blogId'

--- src/common/pipes/trim.pipe.ts
+++ src/common/pipes/trim.pipe.ts
@@ -1,5 +1,8 @@
 import { PipeTransform, Injectable } from '@nestjs/common';
 import { Types } from 'mongoose';
+import { BlogsRepository } from '../../blogs/blogs.repository';
+import { PostInputModel } from '../../posts/post.models';
+
 @Injectable()
 export class TrimPipe implements PipeTransform {
   transform(value: any) {
@@ -14,11 +17,26 @@ export class TrimPipe implements PipeTransform {
   }
 }
 
+@Injectable()
 export class ObjectIdPipe implements PipeTransform {
   transform(value: string): string | false {
     if (typeof value === 'string' && Types.ObjectId.isValid(value)) {
       return value;
     }
     return false;
+  }
+}
+
+@Injectable()
+export class BlogIdPipe implements PipeTransform {
+  constructor(private blogsRepository: BlogsRepository) {}
+  async transform(inputModel: PostInputModel) {
+    const validationBlogId = await this.blogsRepository.findBlogById(
+      inputModel.blogId,
+    );
+    if (!validationBlogId) {
+      return false;
+    }
+    return inputModel;
   }
 }
