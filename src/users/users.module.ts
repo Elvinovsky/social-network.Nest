fd0c5a208c513988c61fb5738902df31928e1187
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-19 19:06:29 +0500

    Update users.module.ts 'add exports provider'

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -14,6 +14,6 @@ import { AuthModule } from '../auth/auth.module';
   ],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository, UsersQueryRepository],
-  exports: [UsersService],
+  exports: [UsersService, UsersQueryRepository],
 })
 export class UsersModule {}
