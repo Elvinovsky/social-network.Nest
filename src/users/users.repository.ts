99e6909a26e13217245ecbb7c56b1a5e133ae871
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-20 21:34:19 +0500

    Update auth.module.ts ref

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,9 +1,4 @@
-import {
-  HttpException,
-  HttpStatus,
-  Injectable,
-  InternalServerErrorException,
-} from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from './users.schema';
 import { Model } from 'mongoose';
@@ -14,6 +9,13 @@ import { objectIdHelper } from '../common/helpers';
 @Injectable()
 export class UsersRepository {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  /**
+   * Получение пользователя по его идентификатору.
+   * @param userId Идентификатор пользователя.
+   * @returns Объект UserViewDTO, представляющий пользователя, или null, если пользователь не найден.
+   * @throws InternalServerErrorException, если возникает ошибка при взаимодействии с базой данных.
+   */
   async getUser(userId: string): Promise<UserViewDTO | null> {
     try {
       if (!objectIdHelper(userId)) return null;
@@ -29,32 +31,64 @@ export class UsersRepository {
     }
   }
 
+  /**
+   * Создание нового пользователя на основе данных из UserCreateDTO.
+   * @param inputModel Данные для создания пользователя.
+   * @returns Объект UserViewDTO, представляющий созданного пользователя.
+   * @throws InternalServerErrorException, если возникает ошибка при сохранении пользователя в базе данных.
+   */
   async createUser(inputModel: UserCreateDTO): Promise<UserViewDTO> {
     const user: UserDocument = new this.userModel(inputModel);
     await user.save();
 
     return userMapping(user);
   }
+
+  /**
+   * Обновление данных пользователя на основе UserInputModel.
+   * @param user Объект пользователя для обновления.
+   * @param inputModel Новые данные пользователя.
+   * @returns Объект Model, представляющий результат операции обновления.
+   * @Throws InternalServerErrorException, если возникает ошибка при взаимодействии с базой данных.
+   */
   async updateUser(user, inputModel: UserInputModel) {
-    return this.userModel.updateOne(
-      { _id: user.id },
-      {
-        $set: {
-          login: inputModel.login,
-          childrenCount: inputModel.email,
+    try {
+      return this.userModel.updateOne(
+        { _id: user.id },
+        {
+          $set: {
+            login: inputModel.login,
+            email: inputModel.email,
+          },
         },
-      },
-    );
+      );
+    } catch (e) {
+      console.log(e);
+      throw new InternalServerErrorException();
+    }
   }
 
+  /**
+   * Удаление пользователя по его идентификатору.
+   * @param userId Идентификатор пользователя.
+   * @returns Объект Document, представляющий результат операции удаления.
+   * @throws InternalServerErrorException, если возникает ошибка при взаимодействии с базой данных.
+   */
   async deleteUserById(userId: string): Promise<Document | null> {
     try {
       return this.userModel.findByIdAndDelete(objectIdHelper(userId));
     } catch (e) {
-      console.log(e, 'error deleteUser method');
-      throw new HttpException('failed', HttpStatus.EXPECTATION_FAILED);
+      console.log(e);
+      throw new InternalServerErrorException();
     }
   }
+
+  /**
+   * Удаление пользователя по адресу электронной почты.
+   * @param email Адрес электронной почты пользователя.
+   * @returns true, если удаление успешно, в противном случае false.
+   * @throws InternalServerErrorException, если возникает ошибка при взаимодействии с базой данных.
+   */
   async deleteUserByEmail(email: string) {
     try {
       const deleteResult = await this.userModel
@@ -62,11 +96,17 @@ export class UsersRepository {
         .exec();
       return deleteResult.deletedCount === 1;
     } catch (e) {
-      console.log(e, 'error deleteUser method');
-      throw new HttpException('failed', HttpStatus.EXPECTATION_FAILED);
+      console.log(e);
+      throw new InternalServerErrorException();
     }
   }
 
+  /**
+   * Поиск пользователя по адресу электронной почты.
+   * @param email Адрес электронной почты пользователя.
+   * @returns Объект UserDocument, представляющий пользователя, или null, если пользователь не найден.
+   * @throws InternalServerErrorException, если возникает ошибка при взаимодействии с базой данных.
+   */
   async findUserByEmail(email: string) {
     try {
       return this.userModel.findOne({ email: email }).exec();
@@ -75,6 +115,12 @@ export class UsersRepository {
       throw new InternalServerErrorException();
     }
   }
+
+  /*** Поиск пользователя по логину.
+   * @param login Логин пользователя.
+   * @returns Объект UserDocument, представляющий пользователя, или null, если пользователь не найден.
+   * @throws InternalServerErrorException, если возникает ошибка при взаимодействии с базой данных.
+   */
   async findUserByLogin(login: string) {
     try {
       return this.userModel.findOne({ login: login }).exec();
@@ -84,6 +130,12 @@ export class UsersRepository {
     }
   }
 
+  /**
+   * Поиск пользователя по коду подтверждения электронной почты.
+   * @param code Код подтверждения электронной почты.
+   * @returns Объект UserDocument, представляющий пользователя, или null, если пользователь не найден.
+   * @throws InternalServerErrorException, если возникает ошибка при взаимодействии с базой данных.
+   */
   async findUserByCode(code: string) {
     try {
       return this.userModel
@@ -95,27 +147,41 @@ export class UsersRepository {
     }
   }
 
+  /**
+   * Поиск пользователя по логину или адресу электронной почты.
+   * @param loginOrEmail Логин или адрес электронной почты пользователя.
+   * @returns Объект UserDocument, представляющий пользователя, или null, если пользователь не найден.
+   * @throws InternalServerErrorException, если возникает ошибка при взаимодействии с базой данных.
+   */
   async findUserLoginOrEmail(loginOrEmail: string) {
     try {
-      // ищем юзера в БД...
+      // Ищем пользователя в базе данных по логину или адресу электронной почты
       const user = await this.userModel
         .findOne({ $or: [{ login: loginOrEmail }, { email: loginOrEmail }] })
         .exec();
 
-      // если не находим возвращаем null.
+      // Если пользователь не найден, возвращаем null
       if (!user) {
         return null;
       }
 
-      //возвращаем найденного юзера.
+      // Возвращаем найденного пользователя
       return user;
     } catch (e) {
       console.log(e);
       throw new InternalServerErrorException();
     }
   }
+
+  /**
+   * Подтверждение адреса электронной почты пользователя по коду.
+   * @param code Код подтверждения электронной почты.
+   * @returns true, если обновление успешно, в противном случае false.
+   * @throws InternalServerErrorException, если возникает ошибка при взаимодействии с базой данных.
+   */
   async confirmEmail(code: string): Promise<boolean> {
     try {
+      // Обновляем статус подтверждения адреса электронной почты пользователя
       const isUpdate = await this.userModel
         .updateOne(
           {
@@ -131,6 +197,13 @@ export class UsersRepository {
     }
   }
 
+  /**
+   * Обновление кода подтверждения электронной почты пользователя по адресу электронной почты.
+   * @param email Адрес электронной почты пользователя.
+   * @param newCode Новый код подтверждения.
+   * @returns true, если обновление успешно, в противном случае false.
+   * @throws InternalServerErrorException, если возникает ошибка при взаимодействии с базой данных.
+   */
   async updateConfirmationCodeByEmail(email: string, newCode: string) {
     try {
       const isUpdate = await this.userModel
@@ -138,7 +211,7 @@ export class UsersRepository {
           {
             email: email,
           },
-          { $set: { emailConfirmation: { confirmationCode: newCode } } },
+          { $set: { 'emailConfirmation.confirmationCode': newCode } },
         )
         .exec();
       return isUpdate.matchedCount === 1;
@@ -147,19 +220,27 @@ export class UsersRepository {
       throw new InternalServerErrorException();
     }
   }
+
+  /**
+   * Поиск пользователя по логину и адресу электронной почты.
+   * @param login Логин пользователя.
+   * @param email Адрес электронной почты пользователя.
+   * @returns Объект UserDocument, представляющий пользователя, или null, если пользователь не найден.
+   * @throws InternalServerErrorException, если возникает ошибка при взаимодействии с базой данных.
+   */
   async findUserByLoginAndEmail(login: string, email: string) {
     try {
-      // ищем юзера в БД...
+      // Ищем пользователя в базе данных по логину или адресу электронной почты
       const user = await this.userModel
         .findOne({ $or: [{ login: login }, { email: email }] })
         .exec();
 
-      // если не находим возвращаем null.
+      // Если пользователь не найден, возвращаем null
       if (!user) {
         return null;
       }
 
-      //возвращаем найденного юзера.
+      // Возвращаем найденного пользователя
       return user;
     } catch (e) {
       console.log(e);
@@ -167,6 +248,13 @@ export class UsersRepository {
     }
   }
 
+  /**
+   * Обновление пароля пользователя по коду и хешу нового пароля.
+   * @param hash Хеш нового пароля.
+   * @param code Код подтверждения.
+   * @returns true, если обновление успешно, в противном случае false.
+   * @throws InternalServerErrorException, если возникает ошибка при взаимодействии с базой данных.
+   */
   async updatePasswordForUser(hash: string, code: string) {
     try {
       const result = await this.userModel
