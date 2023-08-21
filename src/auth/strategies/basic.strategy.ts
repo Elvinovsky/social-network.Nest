7a2cc83deeeb346275fa27decb00181f88e7380e
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-21 20:30:00 +0500

    Update basic.strategy.ts 'fix'

--- src/auth/strategies/basic.strategy.ts
+++ src/auth/strategies/basic.strategy.ts
@@ -2,15 +2,18 @@ import { BasicStrategy as Strategy } from 'passport-http';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ConfigService } from '@nestjs/config';
+import { ConfigType } from '../../configuration/config.module';
 @Injectable()
 export class BasicStrategy extends PassportStrategy(Strategy, 'basic') {
-  constructor(private configService: ConfigService) {
+  constructor(private configService: ConfigService<ConfigType>) {
     super();
   }
   public validate = async (userName, password) => {
     if (
-      this.configService.get('USER_NAME_BASIC_GUARD') === userName &&
-      this.configService.get('PASSWORD_BASIC_GUARD') === password
+      this.configService.get('auth.USER_NAME_BASIC_GUARD', { infer: true }) ===
+        userName &&
+      this.configService.get('auth.PASSWORD_BASIC_GUARD', { infer: true }) ===
+        password
     ) {
       return true;
     }
