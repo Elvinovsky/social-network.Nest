371d15e6b48387b645d959ab891d0bcce6c8f809
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-22 23:50:00 +0500

    Update users.module.ts 'registered CqrsModule'

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,16 +1,18 @@
-import { UsersService } from './users.service';
-import { UsersRepository } from './users.repository';
-import { UsersQueryRepository } from './users.query.repo';
+import { UsersService } from './aplication/users.service';
+import { UsersRepository } from './infrastructure/users.repository';
+import { UsersQueryRepository } from './infrastructure/users.query.repo';
 import { User, UserSchema } from './users.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
-import { UserRegistrationToAdminUseCase } from './use-cases/user-registration-to-admin-use-case.service';
+import { UserRegistrationToAdminUseCase } from './aplication/use-cases/user-registration-to-admin-use-case.service';
+import { CqrsModule } from '@nestjs/cqrs';
 
 const useCases = [UserRegistrationToAdminUseCase];
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    CqrsModule,
   ],
   controllers: [UsersController],
   providers: [...useCases, UsersService, UsersRepository, UsersQueryRepository],
