a8d2ff838c3143dc0d3620dd66879c67909e042f
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-28 22:51:07 +0500

    Update sa-blogs.controller.ts

--- src/blogs/api/sa/sa-blogs.controller.ts
+++ src/blogs/api/sa/sa-blogs.controller.ts
@@ -1,6 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { BasicAuthGuard } from '../../../auth/guards/basic-auth.guard';
+import {
+  QueryInputModel,
+  SearchNameTerm,
+} from '../../../pagination/pagination.models';
+import { BlogsQueryRepo } from '../../infrastructure/repositories/blogs.query.repo';
 
 @Controller('sa/blogs')
 export class SaBlogsController {
-  constructor() {}
+  constructor(private blogsQueryRepo: BlogsQueryRepo) {}
+  @Get()
+  @UseGuards(BasicAuthGuard)
+  async getBlogs(@Query() query: QueryInputModel & SearchNameTerm) {
+    return this.blogsQueryRepo.getSortedBlogs();
+  }
 }
