82a18a2b35966f475e84c7543f1e77cae9d9e08f
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-29 23:18:33 +0500

    Update sa-blogs.controller.ts ' added put endpoint for bind with User '

--- src/blogs/api/sa/sa-blogs.controller.ts
+++ src/blogs/api/sa/sa-blogs.controller.ts
@@ -1,17 +1,77 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Put,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { BasicAuthGuard } from '../../../auth/guards/basic-auth.guard';
 import {
   QueryInputModel,
   SearchNameTerm,
 } from '../../../pagination/pagination.models';
 import { BlogsQueryRepo } from '../../infrastructure/repositories/blogs.query.repo';
+import { ObjectIdPipe } from '../../../common/pipes/object-id.pipe';
+import { UsersService } from '../../../users/aplication/users.service';
+import { BlogsService } from '../../application/blogs.service';
 
 @Controller('sa/blogs')
 export class SaBlogsController {
-  constructor(private blogsQueryRepo: BlogsQueryRepo) {}
+  constructor(
+    private blogsQueryRepo: BlogsQueryRepo,
+    private blogsService: BlogsService,
+    private usersService: UsersService,
+  ) {}
   @Get()
   @UseGuards(BasicAuthGuard)
   async getBlogs(@Query() query: QueryInputModel & SearchNameTerm) {
-    return this.blogsQueryRepo.getSortedBlogs();
+    return this.blogsQueryRepo.getSortedBlogsForSA();
+  }
+
+  @Put(':id/bind-with-user/:userId')
+  @UseGuards(BasicAuthGuard)
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async bindWithUser(
+    @Param('id', ObjectIdPipe) id: string,
+    @Param('userId') userId: string,
+  ) {
+    const foundBlog = await this.blogsService.findById(id);
+
+    if (!foundBlog) {
+      throw new BadRequestException([
+        {
+          field: 'blogId',
+          message: 'Blog with given id does not exist',
+        },
+      ]);
+    } else if (foundBlog.blogOwnerInfo.userId) {
+      throw new BadRequestException([
+        {
+          field: 'blogId',
+          message: 'Blog already bound to user',
+        },
+      ]);
+    }
+
+    const foundUser = await this.usersService.getUser(userId);
+
+    if (!foundUser) {
+      throw new BadRequestException([
+        {
+          field: 'userId',
+          message: 'User with given id does not exist',
+        },
+      ]);
+    }
+    const userInfo = {
+      userId: foundUser.id,
+      userLogin: foundUser.login,
+    };
+    const bind = await this.blogsService.bindWithUser(userInfo, id);
+    return bind;
   }
 }
