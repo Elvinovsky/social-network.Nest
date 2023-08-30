8e9fe22e313f4e7d50d35392ebc4bfcfa9a46a70
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-30 17:00:37 +0500

    Update pagination.models.ts ' create QueryBanStatus'

--- src/pagination/pagination.models.ts
+++ src/pagination/pagination.models.ts
@@ -1,5 +1,5 @@
 import { Transform } from 'class-transformer';
-import { IsOptional } from 'class-validator';
+import { IsOptional, Matches } from 'class-validator';
 
 export type PaginatorType<T> = {
   pagesCount: number;
@@ -24,6 +24,12 @@ export class QueryInputModel {
   @IsOptional()
   sortDirection: string;
 }
+export class QueryBanStatus {
+  @IsOptional()
+  @Matches(/^banned$|^notBanned$/)
+  banStatus: string;
+}
+
 export type SearchNameTerm = { searchNameTerm?: string };
 export type SearchTitleTerm = { searchTitleTerm?: string };
 export type SearchEmailTerm = { searchEmailTerm?: string };
