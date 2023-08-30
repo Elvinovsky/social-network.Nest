4687991b87d0457276621c14c00f27bc322ea032
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-30 16:57:12 +0500

    Create sa-users.controller.ts

---
+++ src/api/sa/sa-users.controller.ts
@@ -0,0 +1,109 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { BasicAuthGuard } from '../../auth/guards/basic-auth.guard';
+import { BanUserInputModel, UserInputModel } from '../../users/user.models';
+import { ResultsAuthForErrors } from '../../auth/auth.constants';
+import { UserRegistrationToAdminCommand } from '../../users/aplication/use-cases/user-registration-to-admin-use-case';
+import { CommandBus } from '@nestjs/cqrs';
+import { UsersService } from '../../users/aplication/users.service';
+import {
+  QueryBanStatus,
+  QueryInputModel,
+  SearchEmailTerm,
+  SearchLoginTerm,
+} from '../../pagination/pagination.models';
+import { UsersQueryRepository } from '../../users/infrastructure/users.query.repo';
+
+@Controller('sa/users')
+export class SaUsersController {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly usersQueryRepo: UsersQueryRepository,
+    private commandBus: CommandBus,
+  ) {}
+
+  @UseGuards(BasicAuthGuard)
+  @Post()
+  async createUser(@Body() inputModel: UserInputModel) {
+    //ищем юзера в БД по эл/почте
+    const isUserExists: true | ResultsAuthForErrors =
+      await this.usersService._isUserExists(inputModel);
+
+    // если находим совпадения по емайлу возвращаем в ответе ошибку.
+    if (isUserExists === ResultsAuthForErrors.email) {
+      throw new BadRequestException([
+        {
+          field: 'email',
+          message: 'email already exists',
+        },
+      ]);
+    }
+
+    // если находим совпадения по логину возвращаем в ответе ошибку.
+    if (isUserExists === ResultsAuthForErrors.login) {
+      throw new BadRequestException([
+        {
+          field: 'login',
+          message: 'login already exists',
+        },
+      ]);
+    }
+
+    //регистрируем юзера в БД.
+    return this.commandBus.execute(
+      new UserRegistrationToAdminCommand(inputModel),
+    );
+  }
+
+  @Get()
+  async getUsers(
+    @Query()
+    query: QueryInputModel & SearchEmailTerm & SearchLoginTerm & QueryBanStatus,
+  ) {
+    return this.usersQueryRepo.getSortedUsersForSA(
+      query.banStatus,
+      query.searchEmailTerm,
+      query.searchLoginTerm,
+      query.pageNumber,
+      query.pageSize,
+      query.sortBy,
+      query.sortDirection,
+    );
+  }
+  @Put(':id/ban')
+  @UseGuards(BasicAuthGuard)
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async updateBanStatus(
+    @Param('id') userId: string,
+    @Body() inputModel: BanUserInputModel,
+  ) {
+    const result = await this.usersService.banUser(userId, inputModel);
+
+    if (result === null) {
+      throw new NotFoundException();
+    }
+
+    if (result === false) {
+      throw new BadRequestException([
+        {
+          field: 'isBanned',
+          message: 'user already',
+        },
+      ]);
+    }
+
+    return result;
+  }
+}
