f96999c8b039cb84608998f71553b661186a1d39
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-09-01 21:19:56 +0500

    Update users.module.ts ' temporarily implemented cyclic dependency between user and device modules '

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,16 +3,18 @@ import { UsersRepository } from './infrastructure/users.repository';
 import { UsersQueryRepository } from './infrastructure/users.query.repo';
 import { User, UserSchema } from './users.schema';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UserRegistrationToAdminUseCase } from './aplication/use-cases/user-registration-to-admin-use-case';
 import { CqrsModule } from '@nestjs/cqrs';
 import { SaUsersController } from '../api/sa/sa-users.controller';
+import { DevicesModule } from '../devices/devices.module';
 
 const useCases = [UserRegistrationToAdminUseCase];
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    forwardRef(() => DevicesModule),
     CqrsModule,
   ],
   controllers: [UsersController, SaUsersController],
