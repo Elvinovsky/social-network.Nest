ba161f40a38e3afc6ff2d5726d8a241e380cc44a
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-09-02 23:55:22 +0500

    Update posts.repository.ts 'registered LikesService, LikesRepository'

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -9,16 +9,29 @@ import { UserRegistrationToAdminUseCase } from './aplication/use-cases/user-regi
 import { CqrsModule } from '@nestjs/cqrs';
 import { SaUsersController } from '../api/sa/sa-users.controller';
 import { DevicesModule } from '../devices/devices.module';
+import { LikesService } from '../likes/likes.service';
+import { LikesRepository } from '../likes/likes.repository';
+import { Like, LikeSchema } from '../likes/like.schemas';
 
 const useCases = [UserRegistrationToAdminUseCase];
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Like.name, schema: LikeSchema },
+    ]),
     forwardRef(() => DevicesModule),
     CqrsModule,
   ],
   controllers: [UsersController, SaUsersController],
-  providers: [...useCases, UsersService, UsersRepository, UsersQueryRepository],
+  providers: [
+    ...useCases,
+    LikesService,
+    LikesRepository,
+    UsersService,
+    UsersRepository,
+    UsersQueryRepository,
+  ],
   exports: [UsersService, UsersQueryRepository],
 })
 export class UsersModule {}
