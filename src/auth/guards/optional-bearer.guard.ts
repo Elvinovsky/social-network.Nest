06a7c716577bcb1966ce55c0d244df323a37d01d
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-09-02 16:18:08 +0500

    Update optional-bearer.guard.ts 'fixed error context'

--- src/auth/guards/optional-bearer.guard.ts
+++ src/auth/guards/optional-bearer.guard.ts
@@ -3,39 +3,47 @@ import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 import { ConfigService } from '@nestjs/config';
 import { ConfigType } from '../../configuration/getConfiguration';
-
-import { SetMetadata } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-//todo transfer
-export const IS_PUBLIC_KEY = 'isPublic';
-export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
+
 @Injectable()
 export class OptionalBearerGuard implements CanActivate {
   constructor(
     private jwtService: JwtService,
     private configService: ConfigService<ConfigType>,
-    private reflector: Reflector, //todo почитать за рефлектор
+    private reflector: Reflector,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = await this.reflector.getAllAndOverride(IS_PUBLIC_KEY, [
+      context.getHandler,
+      context.getClass,
+    ]);
+    if (isPublic) {
+      return true;
+    }
     const request = context.switchToHttp().getRequest();
     const accessToken = this.extractTokenFromHeaders(request);
 
     if (!accessToken) {
       return true;
-    } else {
-      const payload = await this.jwtService
-        .verifyAsync(accessToken, {
-          secret: this.configService.get('auth.SECRET_ACCESS_KEY', {
-            infer: true,
-          }),
-        })
-        .catch(() => {
-          return true;
-        });
-      request.userId = payload.userId;
     }
 
+    const payload = await this.jwtService
+      .verifyAsync(accessToken, {
+        secret: this.configService.get('auth.SECRET_ACCESS_KEY', {
+          infer: true,
+        }),
+      })
+      .catch(() => {
+        return true;
+      });
+
+    if (!payload) {
+      return true;
+    }
+
+    request.userId = payload.userId;
     return true;
   }
 
