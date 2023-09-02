b16872235adaeb0a0fcefd04070522d4af2a6fbc
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-09-02 05:39:10 +0500

    Update optional-bearer.guard.ts 'incomplete implementation'

--- src/auth/guards/optional-bearer.guard.ts
+++ src/auth/guards/optional-bearer.guard.ts
@@ -2,12 +2,19 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 import { ConfigService } from '@nestjs/config';
+import { ConfigType } from '../../configuration/getConfiguration';
 
+import { SetMetadata } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+//todo transfer
+export const IS_PUBLIC_KEY = 'isPublic';
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
 @Injectable()
 export class OptionalBearerGuard implements CanActivate {
   constructor(
     private jwtService: JwtService,
-    private configService: ConfigService,
+    private configService: ConfigService<ConfigType>,
+    private reflector: Reflector, //todo почитать за рефлектор
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
@@ -19,7 +26,9 @@ export class OptionalBearerGuard implements CanActivate {
     } else {
       const payload = await this.jwtService
         .verifyAsync(accessToken, {
-          secret: this.configService.get('SECRET_REFRESH_KEY'),
+          secret: this.configService.get('auth.SECRET_ACCESS_KEY', {
+            infer: true,
+          }),
         })
         .catch(() => {
           return true;
