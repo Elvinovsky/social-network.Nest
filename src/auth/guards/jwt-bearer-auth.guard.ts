305d5f8846d49db43736c45cd42c12a6685ae8ec
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-09-02 16:15:29 +0500

    Update devices.repository.ts 'implemented according to the swagger logic '

--- src/auth/guards/jwt-bearer-auth.guard.ts
+++ src/auth/guards/jwt-bearer-auth.guard.ts
@@ -10,12 +10,7 @@ import { JwtService } from '@nestjs/jwt';
 import { ConfigType } from '../../configuration/getConfiguration';
 import { ConfigService } from '@nestjs/config';
 import { Request } from 'express';
-import { Reflector } from '@nestjs/core';
-
-import { SetMetadata } from '@nestjs/common';
-//todo transfer
-export const IS_PUBLIC_KEY = 'isPublic';
-export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+import { UserInfo } from '../../users/user.models';
 
 @Injectable()
 export class JwtBearerGuard implements CanActivate {
@@ -24,33 +19,41 @@ export class JwtBearerGuard implements CanActivate {
     private usersService: UsersService,
     private jwtService: JwtService,
     private configService: ConfigService<ConfigType>,
-    private reflector: Reflector,
   ) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-    if (isPublic) {
-      // 💡 See this condition
-      return true;
-    }
-
     const request = context.switchToHttp().getRequest();
     const accessToken = this.extractTokenFromHeaders(request);
 
     if (!accessToken) {
       throw new UnauthorizedException();
     } else {
-      const payload = await this.jwtService
+      const payload = (await this.jwtService
         .verifyAsync(accessToken, {
           secret: this.configService.get('auth.SECRET_ACCESS_KEY', {
             infer: true,
           }),
         })
         .catch(() => {
-          return false;
-        });
+          throw new UnauthorizedException();
+        })) as {
+        userInfo: UserInfo;
+        deviceId: string;
+      };
+      if (!payload) {
+        throw new UnauthorizedException();
+      }
+
+      const isBanned = await this.usersService.findUser(
+        payload.userInfo.userId,
+      );
+
+      const sessionActivateCheck =
+        await this.devicesService.findSessionByDeviceId(payload.deviceId);
+
+      if (isBanned?.banInfo.isBanned || !sessionActivateCheck) {
+        throw new UnauthorizedException();
+      }
+
       request.user = {
         userInfo: payload.userInfo,
         deviceId: payload.deviceId,
