bd39025d70a10b3ad1b3779fa6c0784017d9a718
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-09-03 13:32:22 +0500

    Update comment.schemas.ts 'changing the build of a new comment using userinfo'

--- src/comments/comment.schemas.ts
+++ src/comments/comment.schemas.ts
@@ -1,6 +1,7 @@
 import { HydratedDocument, Model } from 'mongoose';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { CommentatorInfo, CommentCreateDTO } from './comment.models';
+import { UserInfo } from '../users/user.models';
 export type CommentDocument = HydratedDocument<Comment>;
 export type CommentModel = Model<Comment>;
 
@@ -19,12 +20,16 @@ export class Comment {
   addedAt: string;
   static create(
     postId: string,
-    commentatorInfo: CommentatorInfo,
+    userInfo: UserInfo,
     content: string,
   ): CommentCreateDTO {
     const newComment: Comment = new Comment();
     newComment.postId = postId;
-    newComment.commentatorInfo = commentatorInfo;
+    newComment.commentatorInfo = {
+      userId: userInfo.userId,
+      userLogin: userInfo.userLogin,
+      isBanned: false,
+    };
     newComment.content = content;
     newComment.addedAt = new Date().toISOString();
 
