81b0ac5406ba6130357f5d467b9b0889c16b432e
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-09-03 13:34:35 +0500

    Update comments.controller.ts 'deleted condition for returning exception response  validation to user ban'

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -42,15 +42,6 @@ export class CommentsController {
       throw new NotFoundException();
     }
 
-    // todo перенести в сервис слой
-    const user = await this.usersService.findUser(
-      comment.commentatorInfo.userId,
-    );
-
-    if (user?.banInfo.isBanned) {
-      throw new NotFoundException();
-    }
-
     return comment;
   }
 
