3c41429759022d4c27189e657d62062e2448a6cc
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-09-05 23:28:54 +0500

    Create current-session-info-from-cookie-jwt.ts

---
+++ src/auth/decorators/current-session-info-from-cookie-jwt.ts
@@ -0,0 +1,23 @@
+import {
+  createParamDecorator,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+
+export const CurrentSessionInfoFromRefreshJWT = createParamDecorator(
+  (data: unknown, context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest();
+
+    const sessionInfo = request.user;
+    if (!sessionInfo.userInfo) {
+      throw new UnauthorizedException();
+    }
+    if (!sessionInfo.deviceId) {
+      throw new UnauthorizedException();
+    }
+    if (!sessionInfo.issuedAt) {
+      throw new UnauthorizedException();
+    }
+    return sessionInfo;
+  },
+);
