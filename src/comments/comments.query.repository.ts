06bda911544909f3b7019a0a02c0bdb770b3a8cb
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-09-08 23:07:04 +0500

    Update comments.query.repository.ts " the method for returning all blogger comments has been restored"

--- src/comments/comments.query.repository.ts
+++ src/comments/comments.query.repository.ts
@@ -1,4 +1,7 @@
-import { PaginatorType } from '../pagination/pagination.models';
+import {
+  PaginatorType,
+  QueryInputModel,
+} from '../pagination/pagination.models';
 import { CommentViewDTO } from './comment.models';
 import { InjectModel } from '@nestjs/mongoose';
 import { Comment, CommentDocument, CommentModel } from './comment.schemas';
@@ -13,12 +16,16 @@ import {
   pagesCountOfBlogs,
 } from '../pagination/pagination.helpers';
 import { DEFAULT_PAGE_SortBy } from '../common/constants';
+import { UserInfo } from '../users/user.models';
+import { objectIdHelper } from '../common/helpers';
+import { BlogsQueryRepo } from '../blogs/infrastructure/repositories/blogs.query.repo';
 
 export class CommentsQueryRepo {
   constructor(
     @InjectModel(Comment.name) private commentModel: CommentModel,
     @InjectModel(Post.name) private postModel: PostModel,
     private readonly commentMapper: CommentMapper,
+    private readonly blogsQueryRepo: BlogsQueryRepo,
   ) {}
   async getCommentsByPostId(
     postId: string,
@@ -54,69 +61,68 @@ export class CommentsQueryRepo {
     };
   }
 
-  // async getAllCommentsForCurrentBlogger(
-  //   query: QueryInputModel,
-  //   userInfo: UserInfo,
-  // ) {
-  //   debugger;
-  //   const blogs = await this.blogsQueryRepo.getSortedBlogsForCurrentBlogger(
-  //     userInfo,
-  //   );
-  //   const blogsIdSorted = blogs.items.map((b) => b.id);
-  //   const postsIdFilter = async (blogsIdArr) => {
-  //     const postsId: any = [];
-  //
-  //     for (let i = 0; i < blogsIdArr.length; i++) {
-  //       const posts = await this.postModel
-  //         .find({
-  //           blogId: objectIdHelper(blogsIdArr[i]),
-  //         })
-  //         .exec();
-  //       if (posts) {
-  //         await postsId.push(posts.map((p) => p.id));
-  //       }
-  //     }
-  //
-  //     return postsId;
-  //   };
-  //   let quantityDocument = 0;
-  //   const itemsComments: CommentDocument[][] = [];
-  //   const postsID = await postsIdFilter(blogsIdSorted);
-  //   const postsIdSorted = postsID.flat();
-  //
-  //   for (let i = 0; i < postsIdSorted.length; i++) {
-  //     const calculateOfFiles = await this.commentModel.countDocuments({
-  //       postId: postsIdSorted[i],
-  //       'commentatorInfo.isBanned': { $ne: true },
-  //     });
-  //
-  //     const foundComments: CommentDocument[] = await this.commentModel
-  //       .find({
-  //         postId: postsIdSorted[i],
-  //         'commentatorInfo.isBanned': { $ne: true },
-  //       })
-  //       .sort({
-  //         [getSortBy(query.sortBy)]: getDirection(query.sortDirection),
-  //         [DEFAULT_PAGE_SortBy]: getDirection(query.sortDirection),
-  //       })
-  //       .skip(
-  //         getSkip(getPageNumber(query.pageNumber), getPageSize(query.pageSize)),
-  //       )
-  //       .limit(getPageSize(query.pageSize));
-  //
-  //     quantityDocument += calculateOfFiles;
-  //     itemsComments.push(foundComments);
-  //   }
-  //
-  //   return {
-  //     pagesCount: pagesCountOfBlogs(quantityDocument, query.pageSize),
-  //     page: getPageNumber(query.pageNumber),
-  //     pageSize: getPageSize(query.pageSize),
-  //     totalCount: quantityDocument,
-  //     items: await this.commentMapper.comments(
-  //       itemsComments.flat(),
-  //       userInfo.userId,
-  //     ),
-  //   };
-  // }
+  async getAllCommentsForCurrentBlogger(
+    query: QueryInputModel,
+    userInfo: UserInfo,
+  ) {
+    const blogs = await this.blogsQueryRepo.getSortedBlogsForCurrentBlogger(
+      userInfo,
+    );
+    const blogsIdSorted = blogs.items.map((b) => b.id);
+    const postsIdFilter = async (blogsIdArr) => {
+      const postsId: any = [];
+
+      for (let i = 0; i < blogsIdArr.length; i++) {
+        const posts = await this.postModel
+          .find({
+            blogId: objectIdHelper(blogsIdArr[i]),
+          })
+          .exec();
+        if (posts) {
+          await postsId.push(posts.map((p) => p.id));
+        }
+      }
+
+      return postsId;
+    };
+    let quantityDocument = 0;
+    const itemsComments: CommentDocument[][] = [];
+    const postsID = await postsIdFilter(blogsIdSorted);
+    const postsIdSorted = postsID.flat();
+
+    for (let i = 0; i < postsIdSorted.length; i++) {
+      const calculateOfFiles = await this.commentModel.countDocuments({
+        postId: postsIdSorted[i],
+        'commentatorInfo.isBanned': { $ne: true },
+      });
+
+      const foundComments: CommentDocument[] = await this.commentModel
+        .find({
+          postId: postsIdSorted[i],
+          'commentatorInfo.isBanned': { $ne: true },
+        })
+        .sort({
+          [getSortBy(query.sortBy)]: getDirection(query.sortDirection),
+          [DEFAULT_PAGE_SortBy]: getDirection(query.sortDirection),
+        })
+        .skip(
+          getSkip(getPageNumber(query.pageNumber), getPageSize(query.pageSize)),
+        )
+        .limit(getPageSize(query.pageSize));
+
+      quantityDocument += calculateOfFiles;
+      itemsComments.push(foundComments);
+    }
+
+    return {
+      pagesCount: pagesCountOfBlogs(quantityDocument, query.pageSize),
+      page: getPageNumber(query.pageNumber),
+      pageSize: getPageSize(query.pageSize),
+      totalCount: quantityDocument,
+      items: await this.commentMapper.comments(
+        itemsComments.flat(),
+        userInfo.userId,
+      ),
+    };
+  }
 }
