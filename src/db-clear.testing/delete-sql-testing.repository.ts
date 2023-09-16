8e87f8154fa37a5b89f8dc2dada89c47a713123a
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-09-16 06:35:21 +0500

    Create delete-sql-testing.repository.ts 'application DAL was migrated to raw SQL '

---
+++ src/db-clear.testing/delete-sql-testing.repository.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+
+@Injectable()
+export class DeleteDbSQLRepository {
+  constructor(@InjectDataSource() protected dataSource: DataSource) {}
+  async deleteDB() {
+    await this.dataSource.query(`
+    DELETE FROM "user"."banInfo"
+    `);
+    await this.dataSource.query(`
+    DELETE FROM "user"."emailConfirmation"
+    `);
+    await this.dataSource.query(`
+    DELETE FROM "devices"."sessions"
+    `);
+    await this.dataSource.query(`
+    DELETE FROM "user"."accountData"
+    `);
+  }
+}
