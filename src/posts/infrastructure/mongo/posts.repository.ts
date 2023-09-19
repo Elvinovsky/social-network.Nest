67f343dfd66e6dd0ad994845ed8dab8dfdec7619
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-09-19 23:38:23 +0500

    Create posts-raw-sql.repository.ts " create raw sql repository for crud operations posts "

---
+++ src/posts/infrastructure/mongo/posts.repository.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Post, PostModel } from '../../post.schemas';
+import {
+  BlogPostInputModel,
+  PostCreateDTO,
+  PostViewDTO,
+} from '../../post.models';
+import { PostMapper } from '../../post.helpers';
+
+@Injectable()
+export class PostsRepository {
+  constructor(
+    @InjectModel(Post.name) private readonly postModel: PostModel,
+    private readonly postMapper: PostMapper,
+  ) {}
+
+  async findPostById(id: string): Promise<boolean> {
+    return !!(await this.postModel.findOne({ id: id }).exec());
+  }
+
+  async createPost(inputModel: PostCreateDTO): Promise<PostViewDTO> {
+    const post = new this.postModel(inputModel);
+    return post
+      .save()
+      .then((post) => this.postMapper.mapPost(post as PostCreateDTO))
+      .catch((error) => Promise.reject(error));
+  }
+
+  async updatePostById(
+    postId: string,
+    blogId: string,
+    inputModel: BlogPostInputModel,
+  ): Promise<boolean> {
+    return this.postModel
+      .updateOne(
+        { id: postId, blogId: blogId },
+        {
+          $set: {
+            title: inputModel.title,
+            shortDescription: inputModel.shortDescription,
+            content: inputModel.content,
+          },
+        },
+      )
+      .then((result) => Promise.resolve(result.matchedCount === 1))
+      .catch((error) => Promise.reject(error));
+  }
+
+  async deletePost(postId: string): Promise<boolean> {
+    return this.postModel
+      .deleteOne({ id: postId })
+      .then((deletedPost) => Promise.resolve(deletedPost.deletedCount === 1))
+      .catch((error) => Promise.reject(error));
+  }
+}
---
+++ src/posts/infrastructure/sql/posts-raw-sql.repository.ts
@@ -0,0 +1,85 @@
+import { Injectable } from '@nestjs/common';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { PostMapper } from '../../post.helpers';
+import {
+  BlogPostInputModel,
+  PostCreateDTO,
+  PostViewDTO,
+} from '../../post.models';
+
+@Injectable()
+export class PostsRawSqlRepository {
+  constructor(
+    @InjectDataSource() protected dataSource: DataSource,
+    private readonly postMapper: PostMapper,
+  ) {}
+
+  async findPostById(postId: string): Promise<boolean> {
+    return this.dataSource
+      .query(
+        `
+    SELECT "title", "blogId"
+    FROM "features"."posts" 
+    WHERE "id" = $1
+    `,
+        [postId],
+      )
+      .then((result) => result.length === 1)
+      .catch((error) => Promise.reject(error));
+  }
+
+  async createPost(inputModel: PostCreateDTO): Promise<PostViewDTO> {
+    await this.dataSource
+      .query(
+        `
+    INSERT INTO "features"."posts"(
+    "id", "title", "shortDescription", "content", "blogId", "blogName", "addedAt")
+        VALUES ($1, $2, $3, $4, $5, $6, $7);
+    `,
+        [
+          inputModel.id,
+          inputModel.title,
+          inputModel.shortDescription,
+          inputModel.content,
+          inputModel.blogId,
+          inputModel.blogName,
+          inputModel.addedAt,
+        ],
+      )
+      .catch((error) => Promise.reject(error));
+
+    return this.postMapper.mapPost(inputModel);
+  }
+
+  async updatePostById(
+    postId: string,
+    blogId: string,
+    inputModel: BlogPostInputModel,
+  ): Promise<boolean> {
+    return this.dataSource
+      .query(
+        `
+      UPDATE "features"."posts"
+      SET  "title" = $3, "shortDescription" = $4, "content"=$5
+      WHERE "id" = $1 and "blogId" = $2;
+      `,
+        [inputModel.title, inputModel.shortDescription, inputModel.content],
+      )
+      .then((result) => result[1] === 1)
+      .catch((error) => error);
+  }
+
+  async deletePost(postId: string): Promise<boolean | null> {
+    return this.dataSource
+      .query(
+        `
+      DELETE FROM features.posts
+      WHERE "id" = $1;
+      `,
+        [postId],
+      )
+      .then((result) => result[1] === 1)
+      .catch((error) => error);
+  }
+}
