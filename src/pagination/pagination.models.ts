baed3811f30c810a39411d5085bb4f902a9eb4b5
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-09-19 08:38:00 +0500

    Update pagination.models.ts " added actions to transform incoming parameters "

--- src/pagination/pagination.models.ts
+++ src/pagination/pagination.models.ts
@@ -1,5 +1,12 @@
 import { Transform } from 'class-transformer';
 import { IsEmail, IsOptional } from 'class-validator';
+import {
+  BanStatus,
+  DEFAULT_PAGE_NUMBER,
+  DEFAULT_PAGE_SIZE,
+  DEFAULT_PAGE_SortBy,
+  SortDirection,
+} from '../common/constants';
 
 export type PaginatorType<T> = {
   pagesCount: number;
@@ -11,22 +18,33 @@ export type PaginatorType<T> = {
 
 export class QueryInputModel {
   @IsOptional()
-  @Transform((value) => Number(value))
+  @Transform(({ value }) =>
+    Number(value) > 0 ? Number(value) : DEFAULT_PAGE_NUMBER,
+  )
   pageNumber: number;
 
   @IsOptional()
-  @Transform((value) => Number(value))
+  @Transform(({ value }) =>
+    Number(value) > 0 ? Number(value) : DEFAULT_PAGE_SIZE,
+  )
   pageSize: number;
 
   @IsOptional()
+  @Transform(({ value }) => (value ? value : DEFAULT_PAGE_SortBy))
   sortBy: string;
 
   @IsOptional()
+  @Transform(({ value }) =>
+    value === 'asc' ? SortDirection.Asc : SortDirection.Desc,
+  )
   sortDirection: string;
 }
 
 export class QueryBanStatus {
   @IsOptional()
+  @Transform(({ value }) =>
+    value === BanStatus.Banned ? BanStatus.Banned : BanStatus.NotBanned,
+  )
   banStatus: string;
 }
 
