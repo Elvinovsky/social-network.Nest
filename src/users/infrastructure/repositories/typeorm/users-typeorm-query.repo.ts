3d04b53a1280fec0f9e6f27fb85d933d874ece11
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-10-08 00:00:17 +0500

    create users-typeorm-query.repo.ts

---
+++ src/users/infrastructure/repositories/typeorm/users-typeorm-query.repo.ts
@@ -0,0 +1,103 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { PaginatorType } from '../../../../infrastructure/pagination/pagination.models';
+import { MeViewModel, UserViewDTO } from '../../../dto/view/user-view.models';
+import { usersMappingSA } from '../../helpers/user.helpers';
+import {
+  getPageNumber,
+  getPageSize,
+  getSkip,
+  getSortBy,
+  pagesCountOfBlogs,
+} from '../../../../infrastructure/pagination/pagination.helpers';
+import { InjectRepository } from '@nestjs/typeorm';
+import {
+  BanInfoTypeOrmEntity,
+  EmailConfirmTypeOrmEntity,
+  UserTypeOrmEntity,
+} from '../../../entities/typeorm/user-sql.schemas';
+import { Repository } from 'typeorm';
+import { UserCreateDTO } from '../../../dto/create/users-create.models';
+
+@Injectable()
+export class UsersTypeormQueryRepo {
+  constructor(
+    @InjectRepository(UserTypeOrmEntity)
+    protected usersRepo: Repository<UserTypeOrmEntity>,
+
+    @InjectRepository(BanInfoTypeOrmEntity)
+    protected banRepo: Repository<BanInfoTypeOrmEntity>,
+
+    @InjectRepository(EmailConfirmTypeOrmEntity)
+    protected emailRepo: Repository<EmailConfirmTypeOrmEntity>,
+  ) {}
+
+  async getSortedUsersForSA(
+    pageNumber: number,
+    pageSize: number,
+    sortBy: string,
+    sortDirection: string,
+    searchEmailTerm?: string,
+    searchLoginTerm?: string,
+    banStatus?: string,
+  ): Promise<PaginatorType<UserViewDTO[]>> {
+    try {
+      const banFilter = (banStatus?: string) => {
+        return banStatus === 'banned' ? true : false;
+      };
+
+      const getEmailTerm = (searchEmailTerm?: string): string =>
+        searchEmailTerm ? `%${searchEmailTerm}%` : `%%`;
+
+      const getLoginTerm = (searchLoginTerm?: string): string =>
+        searchLoginTerm ? `%${searchLoginTerm}%` : `%%`;
+
+      const sortDirectionSQL = (sortDirection?: string) =>
+        sortDirection?.toLowerCase() === 'asc' ? 'ASC' : 'DESC';
+
+      const queryBuilder = this.usersRepo
+        .createQueryBuilder('u')
+        .leftJoinAndSelect('u.banInfo', 'b')
+        .leftJoinAndSelect('u.emailConfirmation', 'e')
+        .where('(u.login ILIKE :login OR u.email ILIKE :email)', {
+          login: getLoginTerm(searchLoginTerm),
+          email: getEmailTerm(searchEmailTerm),
+        })
+        .andWhere('b.isBanned = :isBanned', { isBanned: banFilter(banStatus) })
+        .orderBy('u.' + getSortBy(sortBy), sortDirectionSQL(sortDirection))
+        .offset(getSkip(pageNumber, pageSize))
+        .limit(pageSize);
+
+      console.log(queryBuilder.getSql());
+
+      const [foundUsers, calculateOfFiles] = await Promise.all([
+        queryBuilder.getMany(),
+        queryBuilder.getCount(),
+      ]);
+
+      console.log(foundUsers);
+
+      return {
+        pagesCount: pagesCountOfBlogs(calculateOfFiles, pageSize),
+        page: getPageNumber(pageNumber),
+        pageSize: getPageSize(pageSize),
+        totalCount: calculateOfFiles,
+        items: usersMappingSA(foundUsers as UserCreateDTO[]),
+      };
+    } catch (e) {
+      console.log(e);
+      throw new Error();
+    }
+  }
+
+  async getUserInfo(id: string): Promise<MeViewModel | null> {
+    const user = await this.usersRepo.findOneBy({ id: id });
+    if (!user) {
+      return null;
+    }
+    return {
+      email: user.email,
+      login: user.login,
+      userId: user.id,
+    };
+  }
+}
