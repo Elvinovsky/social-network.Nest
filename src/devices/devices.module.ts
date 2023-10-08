f40527d446b9100b47f81fba68b5762ed620d858
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-10-08 23:27:01 +0500

    "Refactored Device Repositories, Added TypeORM Compatibility, and Injected Dependencies."

--- src/devices/devices.module.ts
+++ src/devices/devices.module.ts
@@ -11,9 +11,13 @@ import { AuthModule } from '../auth/auth.module';
 import { UsersModule } from '../users/users.module';
 import { getConfiguration } from '../infrastructure/configuration/getConfiguration';
 import { DevicesRawSqlRepository } from './infrastructure/repositories/sql/devices-raw-sql.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DeviceTypeOrmEntity } from './entities/typeorm/device-sql.schemas';
+import { DevicesTypeormRepository } from './infrastructure/repositories/typeorm/devices-typeorm.repository';
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature([DeviceTypeOrmEntity]),
     MongooseModule.forFeature([{ name: Device.name, schema: DeviceSchema }]),
     forwardRef(() => AuthModule),
     forwardRef(() => UsersModule),
@@ -26,7 +30,9 @@ import { DevicesRawSqlRepository } from './infrastructure/repositories/sql/devic
       useClass:
         getConfiguration().repo_type === 'Mongo'
           ? DevicesRepository
-          : DevicesRawSqlRepository,
+          : getConfiguration().repo_type === 'sql'
+          ? DevicesRawSqlRepository
+          : DevicesTypeormRepository,
     },
   ],
   exports: [DevicesService],
