482aa97702c59e3b5ff736a2f05b286a870ba3db
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-10-08 23:16:18 +0500

    "Updated imports, data types, and mapping in the devicesMapper function, as well as in the DeviceCreator class to match the new file structure and data types."

--- src/devices/infrastructure/helpers/device.helpers.ts
+++ src/devices/infrastructure/helpers/device.helpers.ts
@@ -1,11 +1,12 @@
-import { DeviceDocument } from '../../entities/mongoose/device-no-sql.schemas';
-import { DeviceViewDTO, SessionCreateDTO } from '../../dto/device.models';
+import {
+  DeviceViewDTO,
+  DeviceWithoutUser,
+  SessionCreateDTO,
+} from '../../dto/device.models';
 import { UserInfo } from '../../../users/dto/view/user-view.models';
 import add from 'date-fns/add';
 
-export const devicesMapper = (
-  array: Array<DeviceDocument>,
-): DeviceViewDTO[] => {
+export const devicesMapper = (array: DeviceWithoutUser[]): DeviceViewDTO[] => {
   return array.map((el) => {
     return {
       ip: el.ip,
